6ba973a289e2ce5b671a664a15402374f43a965b
Robert Jackson <robert.w.jackson@me.com>
2016-03-02 21:44:57 -0500

    Refactor tests to use a single simple DSL.

--- node-tests/unit/linting-compiler-test.js
+++ node-tests/unit/linting-compiler-test.js
@@ -2,27 +2,11 @@
 
 var assert = require('assert');
 var buildTemplateCompiler = require('../helpers/template-compiler');
-var plugins = require('../../ext/plugins');
 
 describe('Ember template compiler', function() {
-  var addonContext;
   var templateCompiler;
-  var messages;
-  var config;
 
   beforeEach(function() {
-    messages = [];
-    config   = {};
-
-    addonContext = {
-      logLintingError: function(pluginName, moduleName, message) {
-        messages.push(message);
-      },
-      loadConfig: function() {
-        return config;
-      }
-    };
-
     templateCompiler = buildTemplateCompiler();
   });
 
@@ -42,24 +26,5 @@ describe('Ember template compiler', function() {
     templateCompiler.registerPlugin('ast', NoopPlugin);
     templateCompiler.precompile('<div></div>');
     assert.equal(instanceCount, 1, 'registered plugins are instantiated');
-  });
-
-  it('prints to the console', function() {
-    templateCompiler.registerPlugin('ast', plugins['bare-strings'](addonContext));
-    templateCompiler.precompile('\n howdy', {
-      moduleName: 'layout.hbs'
-    });
-    assert.deepEqual(messages, ["Non-translated string used (\'layout.hbs\') `\n howdy`"]);
-  });
-
-  it('prints to the console', function() {
-    config['bare-strings'] = false;
-
-    templateCompiler.registerPlugin('ast', plugins['bare-strings'](addonContext));
-    templateCompiler.precompile('\n howdy', {
-      moduleName: 'layout.hbs'
-    });
-
-    assert.deepEqual(messages, []);
   });
 });
--- node-tests/unit/plugins/lint-block-indentation-test.js
+++ node-tests/unit/plugins/lint-block-indentation-test.js
@@ -1,79 +1,19 @@
 'use strict';
 
-var assert = require('assert');
-var buildTemplateCompiler = require('../../helpers/template-compiler');
-var plugins = require('../../../ext/plugins');
+var generateRuleTests = require('../../helpers/rule-test-harness');
 
-describe('Block indentation plugin', function() {
-  var DISABLE_ALL = '<!-- template-lint disable=true -->';
-  var DISABLE_ONE = '<!-- template-lint block-indentation=false -->';
-  var BAD = '\n  {{#each cats as |dog|}}  {{/each}}';
-  var GOOD = '\n  {{#each cats as |dog|}}\n  {{/each}}';
-  var addonContext;
-  var templateCompiler;
-  var messages;
-  var config;
+generateRuleTests({
+  name: 'block-indentation',
 
-  beforeEach(function() {
-    messages = [];
-    config   = {};
+  good: [
+    '\n  {{#each cats as |dog|}}\n  {{/each}}'
+  ],
 
-    addonContext = {
-      logLintingError: function(pluginName, moduleName, message) {
-        messages.push(message);
-      },
-      loadConfig: function() {
-        return config;
-      }
-    };
+  bad: [
+    {
+      template: '\n  {{#each cats as |dog|}}  {{/each}}',
 
-
-    templateCompiler = buildTemplateCompiler();
-  });
-
-  it('logs a message in the console when given incorrect indentation', function() {
-    templateCompiler.registerPlugin('ast', plugins['block-indentation'](addonContext));
-    templateCompiler.precompile(BAD, {
-      moduleName: 'layout.hbs'
-    });
-
-    assert.deepEqual(messages, ["Incorrect `each` block indention at beginning at ('layout.hbs'@ L2:C2)"]);
-  });
-
-  it('passes when given correct indentation', function() {
-    templateCompiler.registerPlugin('ast', plugins['block-indentation'](addonContext));
-    templateCompiler.precompile(GOOD, {
-      moduleName: 'layout.hbs'
-    });
-
-    assert.deepEqual(messages, []);
-  });
-
-  it('rule can be disabled via config', function() {
-    config['block-indentation'] = false;
-    templateCompiler.registerPlugin('ast', plugins['block-indentation'](addonContext));
-    templateCompiler.precompile(BAD, {
-      moduleName: 'layout.hbs'
-    });
-
-    assert.deepEqual(messages, []);
-  });
-
-  it('rule can be disabled via inline comment - single rule', function() {
-    templateCompiler.registerPlugin('ast', plugins['block-indentation'](addonContext));
-    templateCompiler.precompile(DISABLE_ONE + '\n' + BAD, {
-      moduleName: 'layout.hbs'
-    });
-
-    assert.deepEqual(messages, []);
-  });
-
-  it('rule can be disabled via inline comment - all rules', function() {
-    templateCompiler.registerPlugin('ast', plugins['block-indentation'](addonContext));
-    templateCompiler.precompile(DISABLE_ALL + '\n' + BAD, {
-      moduleName: 'layout.hbs'
-    });
-
-    assert.deepEqual(messages, []);
-  });
+      message: "Incorrect `each` block indention at beginning at ('layout.hbs'@ L2:C2)"
+    }
+  ]
 });
