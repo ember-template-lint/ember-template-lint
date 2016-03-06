21deffa3e4dcb5f0e86deb7fb9f70a56c74c3609
Robert Jackson <robert.w.jackson@me.com>
2016-03-06 18:36:35 -0500

    Add broccoli testing setup.

--- broccoli-template-linter.js
+++ broccoli-template-linter.js
@@ -13,6 +13,8 @@ var jsStringEscape = require('js-string-escape');
 var plugins = require('./ext/plugins');
 
 function TemplateLinter(inputNode, _options) {
+  if (!(this instanceof TemplateLinter)) { return new TemplateLinter(inputNode, _options); }
+
   var options = _options || {};
   if (!options.hasOwnProperty('persist')) {
     options.persist = true;
@@ -28,24 +30,31 @@ function TemplateLinter(inputNode, _options) {
   this._console = this.options.console || console;
   this._templatercConfig = undefined;
   this._astPlugins = undefined;
+  this._generateTestFile = this.options.generateTestFile || function() {
+    return '';
+  };
 }
 
 TemplateLinter.prototype = Object.create(Filter.prototype);
 TemplateLinter.prototype.constructor = TemplateLinter;
 
 TemplateLinter.prototype.extensions = ['hbs'];
-TemplateLinter.prototype.targetExtension = '-template-lint-test.js';
+TemplateLinter.prototype.targetExtension = 'template-lint-test.js';
 
 TemplateLinter.prototype.baseDir = function() {
   return __dirname;
 };
 
 TemplateLinter.prototype.loadConfig = function() {
+  if (this._templatercConfig) {
+    return this._templatercConfig;
+  }
+
   var defaultConfigPath = this.options.templatercPath || path.join(process.cwd(), '.template-lintrc');
   var overrideConfigPath = process.env['TEMPLATE_LINTRC'];
   var configPath = overrideConfigPath || defaultConfigPath;
 
-  if(existsSync(configPath)) {
+  if(existsSync(configPath + '.js') || existsSync(configPath + '.json')) {
     this._templatercConfig = require(configPath);
   } else {
     this._templatercConfig = {};
@@ -56,8 +65,9 @@ TemplateLinter.prototype.loadConfig = function() {
 
 TemplateLinter.prototype.cacheKeyProcessString = function(string, relativePath) {
   return md5Hex([
-    string,
     stringify(this.loadConfig()),
+    this._generateTestFile.toString(),
+    string,
     relativePath
   ]);
 };
@@ -91,21 +101,17 @@ TemplateLinter.prototype.processString = function(contents, relativePath) {
     }
   });
 
-  if (this.options.generateTestFile) {
-    var errors = '\n' + this._queuedMessages.join('\n');
-    var passed = this._queuedMessages.length === 0;
+  var errors = '\n' + this._queuedMessages.join('\n');
+  var passed = this._queuedMessages.length === 0;
 
-    return this.options.generateTestFile(
-      'TemplateLint - ' + relativePath,
-      [{
-        name: 'should pass TemplateLint',
-        passed: passed,
-        errorMessage: relativePath + ' should pass TemplateLint.' + jsStringEscape(errors)
-      }]
-    );
-  } else {
-    return '';
-  }
+  return this._generateTestFile(
+    'TemplateLint - ' + relativePath,
+    [{
+      name: 'should pass TemplateLint',
+      passed: passed,
+      errorMessage: relativePath + ' should pass TemplateLint.' + jsStringEscape(errors)
+    }]
+  );
 };
 
 module.exports = TemplateLinter;
