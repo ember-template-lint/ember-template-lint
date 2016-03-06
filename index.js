e718ab3849db3aade97885a51ec7a712ba03770f
Robert Jackson <robert.w.jackson@me.com>
2016-03-05 19:52:14 -0500

    Use HTMLBars compiler directly.
    Removes large issues with caching and shared plugin arrays that occurs
    in Ember's template compiler.  We do not need the output templates to be
    compatible (the AST is perfectly fine for our needs). This makes it
    possible to use this as a stand alone package if we want to extract...

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ var chalk = require('chalk');
 module.exports = {
   name: 'ember-cli-template-lint',
 
-  setupPreprocessorRegistry: function(type, registry) {
-    var plugins = require('./ext/plugins');
-    for (var name in plugins) {
-      registry.add('htmlbars-ast-plugin', {
-        name: name,
-        plugin: plugins[name](this)
-      });
-    }
-  },
-
   loadConfig: function() {
     var defaultConfig  = this.project.root + '/.template-lintrc';
     var overrideConfig = process.env['TEMPLATE_LINTRC'];
