e48d96c2872b2049499b7de50cb8bde12b3fce4d
Robert Jackson <robert.w.jackson@me.com>
2016-03-09 22:07:32 -0500

    Remove requirement that all block statements are multiline.
    Fixes #37.

--- ext/plugins/lint-block-indentation.js
+++ ext/plugins/lint-block-indentation.js
@@ -21,35 +21,15 @@ module.exports = function(addonContext) {
   };
 
   BlockIndentation.prototype.process = function(node) {
-    this['process' + node.type](node);
-  };
-
-  BlockIndentation.prototype.buildWarning = function(name, locationInfo) {
-    return 'Incorrect `' + name + '` block indention at beginning at ' + locationInfo;
-  };
-
-  BlockIndentation.prototype.processElementNode = function(node) {
     // HTML elements that start and end on the same line are fine
     if (node.loc.start.line === node.loc.end.line) {
       return;
     }
 
-    var startColumn = node.loc.start.column;
-    var endColumn   = node.loc.end.column;
-
-    var correctedEndColumn = endColumn - node.tag.length - 3;
-    if(correctedEndColumn !== startColumn) {
-      var startLocation = calculateLocationDisplay(this.options.moduleName, node.loc && node.loc.start);
-      var endLocation = calculateLocationDisplay(this.options.moduleName, node.loc && node.loc.end);
-
-      var warning = 'Incorrect indentation for `' + node.tag + '` beginning at ' + startLocation +
-            '. Expected `</' + node.tag + '>` ending at ' + endLocation + 'to be at an indentation of ' + startColumn + ' but ' +
-            'was found at ' + correctedEndColumn + '.';
-      this.log(warning);
-    }
-  };
-
-  BlockIndentation.prototype.processBlockStatement = function(node) {
+    var isElementNode = node.type === 'ElementNode';
+    var openCloseCharOffset = isElementNode ? 3 : 5;
+    var displayName = isElementNode ? node.tag : node.path.original;
+    var display = isElementNode ? '</' + displayName + '>' : '{{/' + displayName + '}}';
     var startColumn = node.loc.start.column;
     var endColumn   = node.loc.end.column;
 
@@ -58,15 +38,14 @@ module.exports = function(addonContext) {
     }
 
     var startOffset = node._startOffset || 0;
-    var correctedEndColumn = endColumn - node.path.original.length - 5 + startOffset;
+    var correctedEndColumn = endColumn - displayName.length - openCloseCharOffset + startOffset;
     if(correctedEndColumn !== startColumn) {
       var startLocation = calculateLocationDisplay(this.options.moduleName, node.loc && node.loc.start);
       var endLocation = calculateLocationDisplay(this.options.moduleName, node.loc && node.loc.end);
 
-      var warning = 'Incorrect indentation for `' + node.path.original + '` beginning at ' + startLocation +
-            '. Expected `{{/' + node.path.original + '}}` ending at ' + endLocation + 'to be at an indentation of ' + startColumn + ' but ' +
+      var warning = 'Incorrect indentation for `' + displayName + '` beginning at ' + startLocation +
+            '. Expected `' + display + '` ending at ' + endLocation + 'to be at an indentation of ' + startColumn + ' but ' +
             'was found at ' + correctedEndColumn + '.';
-
       this.log(warning);
     }
   };
