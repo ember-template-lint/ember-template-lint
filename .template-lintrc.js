cc56269b5513733131a28917ae2ad72f87d14c98
Gavin Joyce <gavinjoyce@gmail.com>
2016-04-09 23:47:54 +0100

    added acceptance test
    (cherry picked from commit 36da7ce9e606fc1d4ce96c9f187ac9b549c38628)

---
+++ .template-lintrc.js
@@ -0,0 +1,9 @@
+/* jshint node:true */
+'use strict';
+
+module.exports = {
+  'bare-strings': ['(', ')', ',', '.', '&', '+', '-', '=', '*', '/', '#', '%', '!', '?', ':', '[', ']', '{', '}'],
+  'block-indentation': 2,
+  'html-comments': true,
+  'triple-curlies': true
+};
