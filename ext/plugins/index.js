ebf0691c05e1797a032b2fe1f49e627217359458
Gavin Joyce <gavinjoyce@gmail.com>
2016-04-10 07:34:15 +0100

    dont remove config html elements when linting

--- ext/plugins/index.js
+++ ext/plugins/index.js
@@ -4,7 +4,5 @@ module.exports = {
   'bare-strings': require('./lint-bare-strings'),
   'block-indentation': require('./lint-block-indentation'),
   'html-comments': require('./lint-html-comments'),
-  'triple-curlies': require('./lint-triple-curlies'),
-
-  'internal-remove-configuration-html-comments': require('./internal/remove-configuration-html-comments')
+  'triple-curlies': require('./lint-triple-curlies')
 };
