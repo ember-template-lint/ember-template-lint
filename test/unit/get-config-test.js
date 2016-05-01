7acf75de4a9477d08ddc41e517b578a4910901ab
Robert Jackson <robert.w.jackson@me.com>
2016-04-30 21:16:53 -0400

    Add ability to extend internal configuration.

--- test/unit/get-config-test.js
+++ test/unit/get-config-test.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var assert = require('power-assert');
+var assign = require('lodash.assign');
 var getConfig = require('../../lib/get-config');
+var recommendedConfig = require('../../lib/config/recommended');
 
 var fixturePath = path.join(__dirname, '..', 'fixtures');
 var initialCWD = process.cwd();
@@ -40,5 +42,28 @@ describe('get-config', function() {
     });
 
     assert.deepEqual(actual, expected);
+  });
+
+  it('can specify that it extends a default configuration', function() {
+    var actual = getConfig({
+      config: {
+        extends: 'recommended'
+      }
+    });
+
+    assert.deepEqual(actual, recommendedConfig);
+  });
+
+  it('can extend and override a default configuration', function() {
+    var expected = assign({}, recommendedConfig, { 'bare-strings': false });
+    var actual = getConfig({
+      config: {
+        extends: 'recommended',
+        'bare-strings': false
+      }
+    });
+
+    assert.deepEqual(actual, expected);
+    assert(actual['bare-strings'] === false);
   });
 });
