b023b06c0a2918b1a59c48e9aae7af094fcb7222
Ben Holmes <mail@benholmes.uk>
2016-05-27 20:54:59 -0700

    Basic implementation of a rule to check for alt tags

---
+++ test/unit/rules/lint-img-alt-attributes-test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var generateRuleTests = require('../../helpers/rule-test-harness');
+
+generateRuleTests({
+  name: 'img-alt-attributes',
+
+  config: true,
+
+  good: [
+    '<img alt="hullo">'
+  ],
+
+  bad: [
+    {
+      template: '<img>',
+
+      result: {
+        rule: 'img-alt-attributes',
+        message: 'img tags must have an alt attribute',
+        moduleId: 'layout.hbs',
+        source: '<img>',
+        line: 1,
+        column: 0
+      }
+    }
+  ]
+});
