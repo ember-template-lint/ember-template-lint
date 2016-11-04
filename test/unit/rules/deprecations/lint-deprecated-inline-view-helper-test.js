fd300f4b6a7ffdaa9629b99a4dfa0f87f3a133cd
Doug Yun <yun.douglas@gmail.com>
2016-11-04 08:21:47 -0700

    View helper - add more scenarios
    Good:
    
    `{{foo-bar baz=derp}}`
    `<div class="hi" thing={{fish}}`></div>`
    
    Bad:
    
    `{{foo-bar baz=view.derp}}`
    `<div class="hi" thing={{view.fish}}`></div>``

--- test/unit/rules/deprecations/lint-deprecated-inline-view-helper-test.js
+++ test/unit/rules/deprecations/lint-deprecated-inline-view-helper-test.js
@@ -3,7 +3,8 @@
 var generateRuleTests = require('../../../helpers/rule-test-harness');
 var DEPRECATION_URL = require('../../../../lib/rules/deprecations/lint-deprecated-inline-view-helper').DEPRECATION_URL;
 
-var message = 'The inline form of `view` is deprecated. Please use the `Ember.Component` instead. See the deprecation guide at ' + DEPRECATION_URL;
+var message = 'The inline form of `view` is deprecated. Please use the `Ember.Component` instead. ' +
+  'See the deprecation guide at ' + DEPRECATION_URL;
 
 generateRuleTests({
   name: 'deprecated-inline-view-helper',
@@ -59,6 +60,36 @@ generateRuleTests({
         column: 0,
         fix: {
           text: '{{terrible.fishsticks}}'
+        }
+      }
+    },
+    {
+      template: '{{foo-bar bab=good baz=view.qux.qaz boo=okay}}',
+
+      result: {
+        rule: 'deprecated-inline-view-helper',
+        message: message,
+        moduleId: 'layout.hbs',
+        source: '{{foo-bar baz=view.qux.qaz}}',
+        line: 1,
+        column: 19,
+        fix: {
+          text: '{{foo-bar baz=qux.qaz}}'
+        }
+      }
+    },
+    {
+      template: '<div class="whatever-class" data-foo={{view.hallo}} sure=thing></div>',
+
+      result: {
+        rule: 'deprecated-inline-view-helper',
+        message: message,
+        moduleId: 'layout.hbs',
+        source: '<div data-foo={{view.hallo}}></div>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<div data-foo={{hallo}}></div>'
         }
       }
     }
