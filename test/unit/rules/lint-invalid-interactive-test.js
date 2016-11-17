54cc7645a440c138ea794479dee073a60dd3db0c
Francesco Novy <francesco@cropster.com>
2016-11-17 09:40:27 +0100

    Enable submit actions on form elements for invalid-interactive test

--- test/unit/rules/lint-invalid-interactive-test.js
+++ test/unit/rules/lint-invalid-interactive-test.js
@@ -11,6 +11,7 @@ generateRuleTests({
     '<button {{action "foo"}}></button>',
     '<div role="button" {{action "foo"}}></div>',
     '<li><button {{action "foo"}}></button></li>',
+    '<form {{action "foo" on="submit"}}></form>',
     {
       config: { additionalInteractiveTags: ['div'] },
       template: '<div {{action "foo"}}></div>'
@@ -41,6 +42,28 @@ generateRuleTests({
         line: 1,
         column: 5,
         source: '<div onclick={{action \"foo\"}}></div>'
+      }
+    },
+
+    {
+      template: '<form {{action "foo" on="click"}}></form>',
+
+      result: {
+        message: 'Interaction added to non-interactive element',
+        line: 1,
+        column: 6,
+        source: '<form {{action \"foo\" on=\"click\"}}></form>'
+      }
+    },
+
+    {
+      template: '<div {{action "foo" on="submit"}}></div>',
+
+      result: {
+        message: 'Interaction added to non-interactive element',
+        line: 1,
+        column: 5,
+        source: '<div {{action \"foo\" on=\"submit\"}}></div>'
       }
     }
   ]
