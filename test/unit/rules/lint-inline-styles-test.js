1167360e6f50eb2e789e791d9fdeac00d26f8089
jedrula <andrzej.swaton@gmail.com>
2016-12-03 17:54:56 +0100

    add a few bad inline style examples for tests

--- test/unit/rules/lint-inline-styles-test.js
+++ test/unit/rules/lint-inline-styles-test.js
@@ -2,6 +2,21 @@
 
 var generateRuleTests = require('../../helpers/rule-test-harness');
 
+function mapBadStyleExampleToTestRule(inlineStyle) {
+  return {
+    template: '<div ' + inlineStyle + '></div>',
+
+    result: {
+      rule: 'inline-styles',
+      message: 'elements cannot have inline styles',
+      moduleId: 'layout.hbs',
+      source: inlineStyle,
+      line: 1,
+      column: 5
+    }
+  };
+}
+
 generateRuleTests({
   name: 'inline-styles',
 
@@ -14,17 +29,11 @@ generateRuleTests({
   ],
 
   bad: [
-    {
-      template: '<div style="width: 100px"></div>',
-
-      result: {
-        rule: 'inline-styles',
-        message: 'elements cannot have inline styles',
-        moduleId: 'layout.hbs',
-        source: 'style="width: 100px"',
-        line: 1,
-        column: 5
-      }
-    }
-  ]
+    'style="width: 100px"',
+    'style={{foo}}',
+    'style="{{foo}} {{bar}}"',
+    'style',
+    'style=""',
+    'style="color:blue;margin-left:30px;"'
+  ].map(mapBadStyleExampleToTestRule)
 });
