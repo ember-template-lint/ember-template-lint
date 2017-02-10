fb8d5513562491e30ba4dae81228f46cecabcfca
Adrian Zalewski <aazalewski@hotmail.com>
2017-02-10 20:05:50 +0100

    CLI executable spec: fixing script path (adding .js extension).

--- test/unit/bin/ember-template-lint-test.js
+++ test/unit/bin/ember-template-lint-test.js
@@ -8,7 +8,7 @@ describe('ember-template-lint executable', function() {
   describe('basic usage', function() {
     describe('without any parameters', function() {
       it('should exit without error and any console output', function(done) {
-        exec('./bin/ember-template-lint', function(err, stdout, stderr) {
+        exec('./bin/ember-template-lint.js', function(err, stdout, stderr) {
           assert.equal(err, null, 'exits without error');
           assert.equal(stdout, '', 'doesn\'t print anything to standard output');
           assert.equal(stderr, '', 'doesn\'t print anything to error output');
@@ -19,7 +19,7 @@ describe('ember-template-lint executable', function() {
 
     describe('given path to non-existing file', function() {
       it('should exit without error and any console output', function(done) {
-        exec('../../../bin/ember-template-lint app/templates/application-1.hbs', {
+        exec('../../../bin/ember-template-lint.js app/templates/application-1.hbs', {
           cwd: './test/fixtures/with-errors'
         }, function(err, stdout, stderr) {
           assert.equal(err, null, 'exits without error');
@@ -32,7 +32,7 @@ describe('ember-template-lint executable', function() {
 
     describe('given path to single file with errors', function() {
       it('should print errors', function(done) {
-        exec('../../../bin/ember-template-lint app/templates/application.hbs', {
+        exec('../../../bin/ember-template-lint.js app/templates/application.hbs', {
           cwd: './test/fixtures/with-errors'
         }, function(err, stdout, stderr) {
           assert.ok(err, 'exits with error');
@@ -45,7 +45,7 @@ describe('ember-template-lint executable', function() {
 
     describe('given wildcard path resolving to single file', function() {
       it('should print errors', function(done) {
-        exec('../../../bin/ember-template-lint app/templates/*', {
+        exec('../../../bin/ember-template-lint.js app/templates/*', {
           cwd: './test/fixtures/with-errors'
         }, function(err, stdout, stderr) {
           assert.ok(err, 'exits with error');
@@ -58,7 +58,7 @@ describe('ember-template-lint executable', function() {
 
     describe('given directory path', function() {
       it('should print errors', function(done) {
-        exec('../../../bin/ember-template-lint app', {
+        exec('../../../bin/ember-template-lint.js app', {
           cwd: './test/fixtures/with-errors'
         }, function(err, stdout, stderr) {
           assert.ok(err, 'exits with error');
@@ -71,7 +71,7 @@ describe('ember-template-lint executable', function() {
 
     describe('given path to single file without errors', function() {
       it('should exit without error and any console output', function(done) {
-        exec('../../../bin/ember-template-lint app/templates/application.hbs', {
+        exec('../../../bin/ember-template-lint.js app/templates/application.hbs', {
           cwd: './test/fixtures/without-errors'
         }, function(err, stdout, stderr) {
           assert.equal(err, null, 'exits without an error');
@@ -86,7 +86,7 @@ describe('ember-template-lint executable', function() {
   describe('errors formatting', function() {
     describe('without --json param', function() {
       it('should print properly formatted verbose error messages', function(done) {
-        exec('../../../bin/ember-template-lint .', {
+        exec('../../../bin/ember-template-lint.js .', {
           cwd: './test/fixtures/with-errors'
         }, function(err, stdout, stderr) {
           assert.ok(err, 'exits with an error');
@@ -109,7 +109,7 @@ describe('ember-template-lint executable', function() {
 
     describe('with --json param', function() {
       it('should print valid JSON string with errors', function(done) {
-        exec('../../../bin/ember-template-lint . --json', {
+        exec('../../../bin/ember-template-lint.js . --json', {
           cwd: './test/fixtures/with-errors'
         }, function(err, stdout, stderr) {
           var fullTemplateFilePath = path.resolve('./test/fixtures/with-errors/app/templates/application.hbs');
