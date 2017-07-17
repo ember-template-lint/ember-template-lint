2d04b0fc6c82a3487fab3b4b3089c1e83be22a74
akash.dsouza <akash.dsouza@zohocorp.com>
2017-07-17 19:40:40 +0530

    Add require option for self-closing-void-elements

--- test/unit/rules/lint-self-closing-void-elements-test.js
+++ test/unit/rules/lint-self-closing-void-elements-test.js
@@ -23,7 +23,71 @@ generateRuleTests({
     '<param>',
     '<source>',
     '<track>',
-    '<wbr>'
+    '<wbr>',
+    {
+      config: 'require',
+      template: '<area/>'
+    },
+    {
+      config: 'require',
+      template: '<base/>'
+    },
+    {
+      config: 'require',
+      template: '<br/>'
+    },
+    {
+      config: 'require',
+      template: '<col/>'
+    },
+    {
+      config: 'require',
+      template: '<command/>'
+    },
+    {
+      config: 'require',
+      template: '<embed/>'
+    },
+    {
+      config: 'require',
+      template: '<hr/>'
+    },
+    {
+      config: 'require',
+      template: '<img/>'
+    },
+    {
+      config: 'require',
+      template: '<input/>'
+    },
+    {
+      config: 'require',
+      template: '<keygen/>'
+    },
+    {
+      config: 'require',
+      template: '<link/>'
+    },
+    {
+      config: 'require',
+      template: '<meta/>'
+    },
+    {
+      config: 'require',
+      template: '<param/>'
+    },
+    {
+      config: 'require',
+      template: '<source/>'
+    },
+    {
+      config: 'require',
+      template: '<track/>'
+    },
+    {
+      config: 'require',
+      template: '<wbr/>'
+    }
   ],
 
   bad: [
@@ -266,6 +330,262 @@ generateRuleTests({
         column: 0,
         fix: {
           text: '<wbr>'
+        }
+      }
+    },
+    {
+      template: '<area>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<area>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<area/>'
+        }
+      }
+    },
+    {
+      template: '<base>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<base>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<base/>'
+        }
+      }
+    },
+    {
+      template: '<br>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<br>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<br/>'
+        }
+      }
+    },
+    {
+      template: '<col>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<col>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<col/>'
+        }
+      }
+    },
+    {
+      template: '<command>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<command>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<command/>'
+        }
+      }
+    },
+    {
+      template: '<embed>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<embed>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<embed/>'
+        }
+      }
+    },
+    {
+      template: '<hr>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<hr>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<hr/>'
+        }
+      }
+    },
+    {
+      template: '<img>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<img>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<img/>'
+        }
+      }
+    },
+    {
+      template: '<input>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<input>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<input/>'
+        }
+      }
+    },
+    {
+      template: '<keygen>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<keygen>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<keygen/>'
+        }
+      }
+    },
+    {
+      template: '<link>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<link>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<link/>'
+        }
+      }
+    },
+    {
+      template: '<meta>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<meta>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<meta/>'
+        }
+      }
+    },
+    {
+      template: '<param>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<param>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<param/>'
+        }
+      }
+    },
+    {
+      template: '<source>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<source>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<source/>'
+        }
+      }
+    },
+    {
+      template: '<track>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<track>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<track/>'
+        }
+      }
+    },
+    {
+      template: '<wbr>',
+      config: 'require',
+
+      result: {
+        rule: 'self-closing-void-elements',
+        message: 'Self-closing a void element is required',
+        moduleId: 'layout.hbs',
+        source: '<wbr>',
+        line: 1,
+        column: 0,
+        fix: {
+          text: '<wbr/>'
         }
       }
     }
