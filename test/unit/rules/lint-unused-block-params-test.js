5c3216d6600a4bb2cbbd230d60eb187c19b2c0d3
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-24 19:30:46 +0100

    unused-block-params: Disable rule when partial is used in scope

--- test/unit/rules/lint-unused-block-params-test.js
+++ test/unit/rules/lint-unused-block-params-test.js
@@ -10,6 +10,7 @@ generateRuleTests({
   good: [
     '{{cat}}',
     '{{#each cats as |cat|}}{{cat}}{{/each}}',
+    '{{#each cats as |cat|}}{{partial "cat"}}{{/each}}',
     '{{#each cats as |cat|}}{{cat.name}}{{/each}}',
     '{{#each cats as |cat|}}{{meow cat}}{{/each}}',
     '{{#each cats as |cat index|}}{{index}}{{/each}}',
@@ -55,6 +56,19 @@ generateRuleTests({
         source: '{{#each cats as |cat index|}}{{#each cat.lives as |life index|}}{{index}}: {{life}}{{/each}}{{/each}}',
         line: 1,
         column: 0
+      }
+    },    {
+      template:
+      '{{#each cats as |cat index|}}' +
+        '{{partial "cat"}}' +
+        '{{#each cat.lives as |life|}}Life{{/each}}' +
+      '{{/each}}',
+      result: {
+        message: '\'life\' is defined but never used',
+        moduleId: 'layout.hbs',
+        source: '{{#each cat.lives as |life|}}Life{{/each}}',
+        line: 1,
+        column: 46,
       }
     }
   ]
