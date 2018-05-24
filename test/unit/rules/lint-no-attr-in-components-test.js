41020873ee4f2fc60c97436aa49859a815a64950
akash.dsouza <akash.dsouza@zohocorp.com>
2018-05-24 23:16:37 +0530

    Add `no-attr-in-components` rule

---
+++ test/unit/rules/lint-no-attr-in-components-test.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const generateRuleTests = require('../../helpers/rule-test-harness');
+
+generateRuleTests({
+  name: 'no-attr-in-components',
+
+  config: 'true',
+  good: [
+    '<div></div>',
+    '{{foo}}',
+    '<div>{{foo.bar}}</div>',
+    '{{attrs.foo}}', // defaults to layout.hbs
+  ],
+  bad: [
+    {
+      template: '{{attrs.foo}}',
+
+      meta: {
+        moduleId: 'templates/components/layout.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'templates/components/layout.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 2,
+      }
+    },
+    {
+      template: '<div class={{attrs.foo}}></div>',
+
+      meta: {
+        moduleId: 'templates/components/layout.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'templates/components/layout.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 13,
+      }
+    },
+    {
+      template: '{{#if attrs.foo}}bar{{/if}}',
+
+      meta: {
+        moduleId: 'templates/components/layout.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'templates/components/layout.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 6,
+      }
+    },
+    {
+      template: '{{bar foo=attrs.foo}}',
+
+      meta: {
+        moduleId: 'templates/components/layout.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'templates/components/layout.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 10,
+      }
+    },
+    {
+      template: '{{component attrs.foo}}',
+
+      meta: {
+        moduleId: 'templates/components/layout.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'templates/components/layout.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 12,
+      }
+    },
+    {
+      template: '{{bar/baz (hash foo=attrs.foo)}}',
+
+      meta: {
+        moduleId: 'templates/components/layout.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'templates/components/layout.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 20,
+      }
+    },
+    {
+      template: '{{attrs.foo}}',
+
+      meta: {
+        moduleId: 'components/comment/template.hbs',
+      },
+
+      result: {
+        rule: 'no-attr-in-components',
+        message: 'Component templates should not contain `attrs`.',
+        moduleId: 'components/comment/template.hbs',
+        source: 'attrs.foo',
+        line: 1,
+        column: 2,
+      }
+    }
+  ]
+});
