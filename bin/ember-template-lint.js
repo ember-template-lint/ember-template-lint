54314116c8e8c41fc3e81b1c8ff7004caa925977
Jake Dennison <srt4rulez@gmail.com>
2018-06-15 22:09:14 -0400

    Implement quiet opt in cli and update output to show warning count

--- bin/ember-template-lint.js
+++ bin/ember-template-lint.js
@@ -13,21 +13,39 @@ function printErrors(errors) {
   if (process.argv.indexOf('--json') + 1) {
     console.log(JSON.stringify(errors, null, 2));
   } else {
-    let count = 0;
+    const quiet = process.argv.indexOf('--quiet') !== -1;
+
+    let errorCount   = 0;
+    let warningCount = 0;
 
     Object.keys(errors).forEach(filePath => {
-      let options = {};
+      let options = {
+        quiet: quiet
+      };
       let fileErrors = errors[filePath] || [];
-      count += fileErrors.length;
+
+      errorCount   += fileErrors.filter(error => error.severity === Linter.ERROR_SEVERITY).length;
+      warningCount += fileErrors.filter(error => error.severity === Linter.WARNING_SEVERITY).length;
 
       if (process.argv.indexOf('--verbose') + 1) {
         options.verbose = true;
       }
 
-      console.log(Linter.errorsToMessages(filePath, fileErrors, options));
+      const messages = Linter.errorsToMessages(filePath, fileErrors, options);
+      if (messages !== '') {
+        console.log(messages);
+      }
     });
 
-    console.log(chalk.red(chalk.bold(`✖ ${count} problems`)));
+    if (quiet) {
+      warningCount = 0;
+    }
+
+    const count = errorCount + warningCount;
+
+    if (count > 0) {
+      console.log(chalk.red(chalk.bold(`✖ ${count} problems (${errorCount} errors, ${warningCount} warnings)`)));
+    }
   }
 }
 
