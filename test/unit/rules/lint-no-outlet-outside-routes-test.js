fbc499bb01f8a30b074198d0dd26e89630075cee
BnitoBzh <fievet.benoit@gmail.com>
2018-09-11 10:00:07 +0200

    Update tests with more realistic module ids
    Because in an Ember application all module names has the complete template path ie. with the 'app/templates' path.

--- test/unit/rules/lint-no-outlet-outside-routes-test.js
+++ test/unit/rules/lint-no-outlet-outside-routes-test.js
@@ -14,11 +14,11 @@ generateRuleTests({
     {
       template: '{{outlet}}',
       meta: {
-        moduleId: 'foo/route.hbs',
+        moduleId: 'app/templates/foo/route.hbs',
       },
       result: {
         message,
-        moduleId: 'foo/route.hbs',
+        moduleId: 'app/templates/foo/route.hbs',
         source: '{{outlet}}',
         line: 1,
         column: 0
@@ -27,11 +27,11 @@ generateRuleTests({
     {
       template: '{{outlet}}',
       meta: {
-        moduleId: 'routes/foo.hbs'
+        moduleId: 'app/templates/routes/foo.hbs'
       },
       result: {
         message,
-        moduleId: 'routes/foo.hbs',
+        moduleId: 'app/templates/routes/foo.hbs',
         source: '{{outlet}}',
         line: 1,
         column: 0
@@ -40,11 +40,11 @@ generateRuleTests({
     {
       template: '{{#outlet}}Why?!{{/outlet}}',
       meta: {
-        moduleId: 'foo/route.hbs'
+        moduleId: 'app/templates/foo/route.hbs'
       },
       result: {
         message,
-        moduleId: 'foo/route.hbs',
+        moduleId: 'app/templates/foo/route.hbs',
         source: '{{#outlet}}Why?!{{/outlet}}',
         line: 1,
         column: 0
@@ -53,11 +53,11 @@ generateRuleTests({
     {
       template: '{{#outlet}}Why?!{{/outlet}}',
       meta: {
-        moduleId: 'routes/foo.hbs'
+        moduleId: 'app/templates/routes/foo.hbs'
       },
       result: {
         message,
-        moduleId: 'routes/foo.hbs',
+        moduleId: 'app/templates/routes/foo.hbs',
         source: '{{#outlet}}Why?!{{/outlet}}',
         line: 1,
         column: 0
@@ -66,11 +66,11 @@ generateRuleTests({
     {
       template: '{{#outlet}}Works because ambiguous{{/outlet}}',
       meta: {
-        moduleId: 'something/foo.hbs'
+        moduleId: 'app/templates/something/foo.hbs'
       },
       result: {
         message,
-        moduleId: 'something/foo.hbs',
+        moduleId: 'app/templates/something/foo.hbs',
         source: '{{#outlet}}Works because ambiguous{{/outlet}}',
         line: 1,
         column: 0
@@ -82,12 +82,12 @@ generateRuleTests({
       template: '{{outlet}}',
 
       meta: {
-        moduleId: 'components/foo/layout.hbs'
+        moduleId: 'app/templates/components/foo/layout.hbs'
       },
 
       result: {
         message,
-        moduleId: 'components/foo/layout.hbs',
+        moduleId: 'app/templates/components/foo/layout.hbs',
         source: '{{outlet}}',
         line: 1,
         column: 0
@@ -97,12 +97,12 @@ generateRuleTests({
       template: '{{outlet}}',
 
       meta: {
-        moduleId: 'foo/-mything.hbs'
+        moduleId: 'app/templates/foo/-mything.hbs'
       },
 
       result: {
         message,
-        moduleId: 'foo/-mything.hbs',
+        moduleId: 'app/templates/foo/-mything.hbs',
         source: '{{outlet}}',
         line: 1,
         column: 0
