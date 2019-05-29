e606e95ca6b49138f0f0cc715f460f500d021008
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-05-30 02:38:01 +0300

    updates from comments

--- test/unit/rules/lint-require-iframe-title-test.js
+++ test/unit/rules/lint-require-iframe-title-test.js
@@ -19,14 +19,59 @@ generateRuleTests({
     {
       template: '<iframe title="foo" /><iframe title="foo" />',
 
-      result: {
-        message:
-          '<iframe> elements must have a unique title property. Value title="foo" already used for different iframe.',
-        moduleId: 'layout.hbs',
-        source: '<iframe title="foo" />',
-        line: 1,
-        column: 22,
-      },
+      results: [
+        {
+          message: 'This title is not unique. #1',
+          moduleId: 'layout.hbs',
+          source: 'title="foo"',
+          line: 1,
+          column: 8,
+        },
+        {
+          message:
+            '<iframe> elements must have a unique title property. Value title="foo" already used for different iframe. #1',
+          moduleId: 'layout.hbs',
+          source: '<iframe title="foo" />',
+          line: 1,
+          column: 22,
+        },
+      ],
+    },
+    {
+      template:
+        '<iframe title="foo" /><iframe title="boo" /><iframe title="foo" /><iframe title="boo" />',
+      results: [
+        {
+          message: 'This title is not unique. #1',
+          moduleId: 'layout.hbs',
+          source: 'title="foo"',
+          line: 1,
+          column: 8,
+        },
+        {
+          message:
+            '<iframe> elements must have a unique title property. Value title="foo" already used for different iframe. #1',
+          moduleId: 'layout.hbs',
+          source: '<iframe title="foo" />',
+          line: 1,
+          column: 44,
+        },
+        {
+          message: 'This title is not unique. #2',
+          moduleId: 'layout.hbs',
+          source: 'title="boo"',
+          line: 1,
+          column: 30,
+        },
+        {
+          message:
+            '<iframe> elements must have a unique title property. Value title="boo" already used for different iframe. #2',
+          moduleId: 'layout.hbs',
+          source: '<iframe title="boo" />',
+          line: 1,
+          column: 66,
+        },
+      ],
     },
     {
       template: '<iframe src="12" />',
