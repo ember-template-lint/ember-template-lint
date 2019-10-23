3a8291bd50f27b751f5275011167839a5c74f97a
Melanie Sumner <melaniersumner@gmail.com>
2019-10-23 15:29:21 -0500

    test passes

--- test/unit/rules/lint-no-meta-redirect-with-time-limit-test.js
+++ test/unit/rules/lint-no-meta-redirect-with-time-limit-test.js
@@ -7,18 +7,18 @@ generateRuleTests({
 
   config: true,
 
-  good: ['<meta http-equiv="refresh" content="0; url=http://www.example.com" />'],
+  good: ['<meta http-equiv="refresh" content="0; url=http://www.example.com">'],
 
   bad: [
     {
-      template: '<meta http-equiv="refresh" content="1; url=http://www.example.com" />',
+      template: '<meta http-equiv="refresh" content="1; url=http://www.example.com">',
 
       result: {
         moduleId: 'layout.hbs',
         message: 'a meta redirect should not have a delay value greater than zero',
         line: 1,
         column: 0,
-        source: '<meta http-equiv="refresh" content="1; url=http://www.example.com" />',
+        source: '<meta http-equiv="refresh" content="1; url=http://www.example.com">',
       },
     },
   ],
