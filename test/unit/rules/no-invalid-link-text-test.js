b8216f49833b98e814eb8b5bdd85b81896231349
Melanie Sumner <melaniersumner@gmail.com>
2019-12-11 19:12:30 -0600

    added support for link-to

--- test/unit/rules/no-invalid-link-text-test.js
+++ test/unit/rules/no-invalid-link-text-test.js
@@ -7,7 +7,11 @@ generateRuleTests({
 
   config: true,
 
-  good: ['<a href="https://myurl.com">Click here to read more about this amazing adventure</a>'],
+  good: [
+    '<a href="https://myurl.com">Click here to read more about this amazing adventure</a>',
+    '{{#link-to}} click here to read more about our company{{/link-to}}',
+    '<LinkTo>Read more about ways semantic HTML can make your code more accessible.</LinkTo>',
+  ],
 
   bad: [
     {
@@ -16,6 +20,26 @@ generateRuleTests({
         message: 'Links should have descriptive text',
         moduleId: 'layout.hbs',
         source: '<a href="https://myurl.com">click here</a>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<LinkTo>click here</LinkTo>',
+      result: {
+        message: 'Links should have descriptive text',
+        moduleId: 'layout.hbs',
+        source: '<LinkTo>click here</LinkTo>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '{{#link-to}}click here{{/link-to}}',
+      result: {
+        message: 'Links should have descriptive text',
+        moduleId: 'layout.hbs',
+        source: '{{#link-to}}click here{{/link-to}}',
         line: 1,
         column: 0,
       },
