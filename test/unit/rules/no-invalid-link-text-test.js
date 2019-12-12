c5a354d19706c8badd23f0ca809d36c5aead3353
Melanie Sumner <melaniersumner@gmail.com>
2019-12-12 14:35:33 -0600

    added another good example

--- test/unit/rules/no-invalid-link-text-test.js
+++ test/unit/rules/no-invalid-link-text-test.js
@@ -11,6 +11,7 @@ generateRuleTests({
     '<a href="https://myurl.com">Click here to read more about this amazing adventure</a>',
     '{{#link-to}} click here to read more about our company{{/link-to}}',
     '<LinkTo>Read more about ways semantic HTML can make your code more accessible.</LinkTo>',
+    '<LinkTo>{{foo}} more</LinkTo>',
   ],
 
   bad: [
