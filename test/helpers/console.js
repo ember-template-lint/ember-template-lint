0cc346f9e3c0c93a1c93c63541c9766554d0348b
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-13 12:06:30 +0100

    test/console: Reimplement using a real class

--- test/helpers/console.js
+++ test/helpers/console.js
@@ -1,11 +1,19 @@
-function buildFakeConsole() {
-  return {
-    _logLines: [],
+class FakeConsole {
+  constructor() {
+    this._logLines = [];
+  }
 
-    log(data) {
-      this._logLines.push(data);
-    },
-  };
+  log(data) {
+    this._logLines.push(data);
+  }
+
+  get stdout() {
+    return this._logLines.join('\n');
+  }
+}
+
+function buildFakeConsole() {
+  return new FakeConsole();
 }
 
 module.exports = buildFakeConsole;
