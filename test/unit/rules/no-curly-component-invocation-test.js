f8aee849de6ca568d25c5439c2a83da7297ee1b7
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-21 20:12:00 +0100

    test/no-curly-component-invocation: Extract `SHARED_GOOD|BAD` arrays

--- test/unit/rules/no-curly-component-invocation-test.js
+++ test/unit/rules/no-curly-component-invocation-test.js
@@ -6,6 +6,46 @@ function generateError(name) {
   return `You are using the component {{${name}}} with curly component syntax. You should use <${angleBracketName}> instead. If it is actually a helper you must manually add it to the 'no-curly-component-invocation' rule configuration, e.g. \`'no-curly-component-invocation': { allow: ['${name}'] }\`.`;
 }
 
+const SHARED_GOOD = [
+  '<GoodCode />',
+  '<GoodCode></GoodCode>',
+  '{{if someProperty "yay"}}',
+  '<Nested::GoodCode />',
+  '<Nested::GoodCode @someProperty={{-50}} @someProperty={{"-50"}} @someProperty={{true}} />',
+  '{{some-valid-helper param}}',
+  '{{some/valid-nested-helper param}}',
+  '{{@someArg}}',
+  '{{this.someProperty}}',
+  `{{#each items as |item|}}
+     {{item}}
+   {{/each}}`,
+];
+
+const SHARED_BAD = [
+  {
+    template: '{{nested/bad-code}}',
+    results: [getErrorResult(generateError('nested/bad-code'), '{{nested/bad-code}}')],
+  },
+  {
+    template: '{{heading size="1" text="Disallowed heading component"}}',
+    results: [
+      getErrorResult(
+        generateError('heading'),
+        '{{heading size="1" text="Disallowed heading component"}}'
+      ),
+    ],
+  },
+  {
+    template: '{{#heading size="1"}}Disallowed heading component{{/heading}}',
+    results: [
+      getErrorResult(
+        generateError('heading'),
+        '{{#heading size="1"}}Disallowed heading component{{/heading}}'
+      ),
+    ],
+  },
+];
+
 generateRuleTests({
   name: 'no-curly-component-invocation',
 
@@ -15,18 +55,7 @@ generateRuleTests({
   },
 
   good: [
-    '<GoodCode />',
-    '<GoodCode></GoodCode>',
-    '{{if someProperty "yay"}}',
-    '<Nested::GoodCode />',
-    '<Nested::GoodCode @someProperty={{-50}} @someProperty={{"-50"}} @someProperty={{true}} />',
-    '{{some-valid-helper param}}',
-    '{{some/valid-nested-helper param}}',
-    '{{@someArg}}',
-    '{{this.someProperty}}',
-    `{{#each items as |item|}}
-        {{item}}
-     {{/each}}`,
+    ...SHARED_GOOD,
     '{{#-in-element}}Hello{{/-in-element}}',
     '{{#in-element}}Hello{{/in-element}}',
     '{{#some-component foo="bar"}}foo{{else}}bar{{/some-component}}',
@@ -41,36 +70,15 @@ generateRuleTests({
   ],
 
   bad: [
-    {
-      template: '<div>{{bad-code}}</div>',
-      results: [
-        Object.assign(getErrorResult(generateError('bad-code'), '{{bad-code}}'), { column: 5 }),
-      ],
-    },
+    ...SHARED_BAD,
     {
       template: '{{bad-code}}',
       results: [getErrorResult(generateError('bad-code'), '{{bad-code}}')],
     },
     {
-      template: '{{nested/bad-code}}',
-      results: [getErrorResult(generateError('nested/bad-code'), '{{nested/bad-code}}')],
-    },
-    {
-      template: '{{heading size="1" text="Disallowed heading component"}}',
+      template: '<div>{{bad-code}}</div>',
       results: [
-        getErrorResult(
-          generateError('heading'),
-          '{{heading size="1" text="Disallowed heading component"}}'
-        ),
-      ],
-    },
-    {
-      template: '{{#heading size="1"}}Disallowed heading component{{/heading}}',
-      results: [
-        getErrorResult(
-          generateError('heading'),
-          '{{#heading size="1"}}Disallowed heading component{{/heading}}'
-        ),
+        Object.assign(getErrorResult(generateError('bad-code'), '{{bad-code}}'), { column: 5 }),
       ],
     },
   ],
@@ -85,48 +93,13 @@ generateRuleTests({
     requireDash: false,
   },
 
-  good: [
-    '<GoodCode />',
-    '<GoodCode></GoodCode>',
-    '{{if someProperty "yay"}}',
-    '<Nested::GoodCode />',
-    '<Nested::GoodCode @someProperty={{-50}} @someProperty={{"-50"}}/ />',
-    '{{some-valid-helper param}}',
-    '{{some/valid-nested-helper param}}',
-    '{{@someArg}}',
-    '{{this.someProperty}}',
-    `{{#each items as |item|}}
-       {{item}}
-     {{/each}}`,
-    '{{#each items as |item|}}{{item value}}{{/each}}',
-  ],
+  good: [...SHARED_GOOD, '{{#each items as |item|}}{{item value}}{{/each}}'],
 
   bad: [
+    ...SHARED_BAD,
     {
       template: '{{box}}',
       results: [getErrorResult(generateError('box'), '{{box}}')],
-    },
-    {
-      template: '{{nested/bad-code}}',
-      results: [getErrorResult(generateError('nested/bad-code'), '{{nested/bad-code}}')],
-    },
-    {
-      template: '{{heading size="1" text="Disallowed heading component"}}',
-      results: [
-        getErrorResult(
-          generateError('heading'),
-          '{{heading size="1" text="Disallowed heading component"}}'
-        ),
-      ],
-    },
-    {
-      template: '{{#heading size="1"}}Disallowed heading component{{/heading}}',
-      results: [
-        getErrorResult(
-          generateError('heading'),
-          '{{#heading size="1"}}Disallowed heading component{{/heading}}'
-        ),
-      ],
     },
     {
       template: '{{#each items as |item|}} {{item some=args}} {{/each}}',
