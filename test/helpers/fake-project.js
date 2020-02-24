6ff6792d868b488998caee2e2a109f74b2685ec8
Robert Jackson <rjackson@linkedin.com>
2020-02-23 20:49:48 -0500

    Add fake project test helper.
    This simplifies the usage of fixturify style tests by moving from
    `broccoli-test-helper`'s `createTempDir` to using
    [fixturify-project](https://github.com/stefanpenner/node-fixturify-project)
    with a custom subclass (for common functionality).

---
+++ test/helpers/fake-project.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const path = require('path');
+const FixturifyProject = require('fixturify-project');
+
+const ROOT = process.cwd();
+
+module.exports = class FakeProject extends FixturifyProject {
+  constructor() {
+    super('fake-project');
+  }
+
+  setConfig(config) {
+    this.files['.template-lintrc.js'] = `module.exports = ${JSON.stringify(config, null, 2)};`;
+
+    this.writeSync();
+  }
+
+  path(subPath) {
+    return subPath ? path.join(this.baseDir, subPath) : this.baseDir;
+  }
+
+  // behave like a TempDir from broccoli-test-helper
+  write(dirJSON) {
+    Object.assign(this.files, dirJSON);
+    this.writeSync();
+  }
+
+  chdir() {
+    this._dirChanged = true;
+
+    // ensure the directory structure is created initially
+    this.writeSync();
+
+    process.chdir(this.baseDir);
+  }
+
+  dispose() {
+    if (this._dirChanged) {
+      process.chdir(ROOT);
+    }
+
+    return super.dispose();
+  }
+};
