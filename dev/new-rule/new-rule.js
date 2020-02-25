6b149cdd41cd8cf2d01b8be686c74e3a136a2d1c
Melanie Sumner <melaniersumner@gmail.com>
2020-02-25 06:31:02 -0600

    # This is a combination of 20 commits. # This is the 1st commit message:
    Adds ability to create new rule from cli
    
    $ node dev/new-rule/new-rule.js rule-name
    Done in 0.21s. command
    
    removed extra escape character
    
    trying to make the linter ignore the dev/new-rule folder
    
    updating the package.json to tell jest not to test the dev folder.
    
    updating the jest ignore path for the new rules
    
    updating the file structure per code review
    
    updating comments with TODOs
    
    updated eslintrc to ignore dev/new-rule/templates js files
    
    updating paths to omit the slashes per code review
    
    updating rule file to use placeholder class
    
    abstract rule into function
    
    remove unnecessary TODO now that we take care of it in the generating command
    
    tidying up per code review
    
    added chalk to cli message
    
    extracting titlecase into function
    
    fixed linting issues
    
    added help command
    
    adds yarn run v1.16.0
    $ node dev/new-rule/new-rule.js rule-name
    Done in 0.21s. command
    
    removed extra escape character
    
    trying to make the linter ignore the dev/new-rule folder
    
    updating the package.json to tell jest not to test the dev folder.
    
    updating the jest ignore path for the new rules
    
    updating the file structure per code review
    
    updating comments with TODOs
    
    updated eslintrc to ignore dev/new-rule/templates js files
    
    updating paths to omit the slashes per code review
    
    updating rule file to use placeholder class
    
    abstract rule into function
    
    remove unnecessary TODO now that we take care of it in the generating command
    
    tidying up per code review
    
    added chalk to cli message
    
    extracting titlecase into function
    
    fixed linting issues
    
    added help command
    
    # The commit message #2 will be skipped:
    
    # removed extra escape character
    
    # The commit message #3 will be skipped:
    
    # trying to make the linter ignore the dev/new-rule folder
    
    # The commit message #4 will be skipped:
    
    # updating the package.json to tell jest not to test the dev folder.
    
    # The commit message #5 will be skipped:
    
    # updating the jest ignore path for the new rules
    
    # The commit message #6 will be skipped:
    
    # updating the file structure per code review
    
    # The commit message #7 will be skipped:
    
    # updating comments with TODOs
    
    # The commit message #8 will be skipped:
    
    # updated eslintrc to ignore dev/new-rule/templates js files
    
    # The commit message #9 will be skipped:
    
    # updating paths to omit the slashes per code review
    
    # The commit message #10 will be skipped:
    
    # updating rule file to use placeholder class
    
    # The commit message #11 will be skipped:
    
    # abstract rule into function
    
    # The commit message #12 will be skipped:
    
    # remove unnecessary TODO now that we take care of it in the generating command
    
    # The commit message #13 will be skipped:
    
    # tidying up per code review
    
    # The commit message #14 will be skipped:
    
    # added chalk to cli message
    
    # The commit message #15 will be skipped:
    
    # extracting titlecase into function
    
    # The commit message #16 will be skipped:
    
    # fixed linting issues
    
    # The commit message #17 will be skipped:
    
    # added help command
    
    # The commit message #18 will be skipped:
    
    # adds yarn run v1.16.0
    # $ node dev/new-rule/new-rule.js rule-name
    # Done in 0.21s. command
    
    # The commit message #19 will be skipped:
    
    # removed extra escape character
    
    # The commit message #20 will be skipped:
    
    # trying to make the linter ignore the dev/new-rule folder

---
+++ dev/new-rule/new-rule.js
@@ -0,0 +1,58 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable node/no-unsupported-features/node-builtins */
+/* eslint-disable prefer-template */
+
+/**
+node.js-CLI-based generation of new rule template files
+Example usage: 
+--------------
+Templates use the rule name `my-new-rule`, so this example
+creates a functioning/immediately testable rule
+# /path/to/ember-template-lint
+$ yarn new my-new-rule
+$ yarn test my-new-rule
+*/
+
+const fs = require('fs');
+const path = require('path');
+
+// CLI Argument: `my-new-rule` (name of new rule to add)
+let newRuleName = process.argv[2];
+
+// Utility Filesystem: template files co-localized with this script
+let pathUtil = __dirname;
+let pathDocTemplate = path.join(pathUtil, 'new-rule-doc-template.md');
+let pathRuleTemplate = path.join(pathUtil, 'new-rule-template.js');
+let pathTestTemplate = path.join(pathUtil, 'new-rule-test-template.js');
+
+// Parent Filesystem: path/to/ember-template-lint
+let pathRoot = process.cwd();
+let pathDocList = path.join(pathRoot, 'docs/', 'rules.md');
+let pathDocNew = path.join(pathRoot, 'docs/rule/', newRuleName + '.md');
+let pathRuleList = path.join(pathRoot, 'lib/rules/', 'index.js');
+let pathRuleNew = path.join(pathRoot, 'lib/rules/', newRuleName + '.js');
+let pathTestNew = path.join(pathRoot, 'test/unit/rules/', newRuleName + '-test.js');
+let docListAppend = '\n* [' + newRuleName + '](rule/' + newRuleName + '.md)';
+
+fs.copyFileSync(pathDocTemplate, pathDocNew);
+fs.copyFileSync(pathRuleTemplate, pathRuleNew);
+fs.copyFileSync(pathTestTemplate, pathTestNew);
+fs.appendFileSync(pathDocList, docListAppend);
+
+// Feature: inserts new rule information into the rule list for immediate use
+function insertNewRuleIntoList(newRuleName, pathRuleListOrg) {
+  let ruleListInsert = "\n  '" + newRuleName + "': require('./" + newRuleName + "'),";
+  let ruleListPattern = /\n\};/;
+  let orgContent = fs.readFileSync(pathRuleListOrg, { encoding: 'utf8' });
+  let matches = orgContent.match(ruleListPattern);
+  if (matches) {
+    let insertIndex = matches.index;
+    let prepend = orgContent.slice(0, insertIndex);
+    let insert = ruleListInsert;
+    let append = orgContent.slice(insertIndex);
+    let outContent = prepend + insert + append;
+    fs.writeFileSync(pathRuleListOrg, outContent);
+  }
+}
+
+insertNewRuleIntoList(newRuleName, pathRuleList);
