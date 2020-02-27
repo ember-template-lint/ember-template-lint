b966e643b0f1afb05910f2d02b3e372f97b1a51e
Robert Jackson <rjackson@linkedin.com>
2020-02-27 10:09:36 -0500

    Add support for naming test cases.
    Add support for naming specific cases. This makes the test harness use
    the specified test name (as a prefix), instead of using the `template`
    in the name.

---
+++ test/acceptance/rule-test.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const Rule = require('../../lib/rules/base');
+const generateRuleTests = require('../helpers/rule-test-harness');
+
+describe('rule public api', function() {
+  describe('general test harness support', function() {
+    generateRuleTests({
+      plugins: [
+        {
+          name: 'test',
+          rules: {
+            'no-elements': class extends Rule {
+              visitor() {
+                return {
+                  ElementNode(node) {
+                    this.log({
+                      message: 'Do not use any HTML elements!',
+                      line: node.loc && node.loc.start.line,
+                      column: node.loc && node.loc.start.column,
+                      source: this.sourceForNode(node),
+                    });
+                  },
+                };
+              }
+            },
+          },
+        },
+      ],
+
+      name: 'no-elements',
+      config: true,
+
+      good: ['{{haha-wtf}}'],
+
+      bad: [
+        {
+          name: 'uses static result - multiple',
+          template: '<div></div><div></div>',
+          results: [
+            {
+              column: 0,
+              line: 1,
+              message: 'Do not use any HTML elements!',
+              source: '<div></div>',
+            },
+            {
+              column: 11,
+              line: 1,
+              message: 'Do not use any HTML elements!',
+              source: '<div></div>',
+            },
+          ],
+        },
+        {
+          name: 'uses static result - multiple',
+          template: '<div></div>',
+          result: {
+            column: 0,
+            line: 1,
+            message: 'Do not use any HTML elements!',
+            source: '<div></div>',
+          },
+        },
+      ],
+    });
+  });
+});
