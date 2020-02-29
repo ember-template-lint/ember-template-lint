32c8d58eb00919fd5af5740cd2200ad4f561fba9
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-02-29 12:22:34 +0100

    require-button-type: Implement `fix` mode

--- test/unit/rules/require-button-type-test.js
+++ test/unit/rules/require-button-type-test.js
@@ -27,52 +27,62 @@ generateRuleTests({
   bad: [
     {
       template: '<button/>',
+      fixedTemplate: '<button type="button" />',
 
       result: {
         message: ERROR_MESSAGE,
         source: '<button/>',
         line: 1,
         column: 0,
+        isFixable: true,
       },
     },
     {
       template: '<button>label</button>',
+      fixedTemplate: '<button type="button">label</button>',
 
       result: {
         message: ERROR_MESSAGE,
         source: '<button>label</button>',
         line: 1,
         column: 0,
+        isFixable: true,
       },
     },
     {
       template: '<button type="" />',
+      fixedTemplate: '<button type="button" />',
 
       result: {
         message: ERROR_MESSAGE,
         source: '<button type="" />',
         line: 1,
         column: 0,
+        isFixable: true,
       },
     },
     {
       template: '<button type="foo" />',
+      fixedTemplate: '<button type="button" />',
 
       result: {
         message: ERROR_MESSAGE,
         source: '<button type="foo" />',
         line: 1,
         column: 0,
+        isFixable: true,
       },
     },
     {
       template: '<button type=42 />',
+      fixedTemplate: '<button type="button" />',
 
       result: {
         message: ERROR_MESSAGE,
         source: '<button type=42 />',
         line: 1,
         column: 0,
+        isFixable: true,
       },
     },
   ],
