3bc6d32d48ae66e57c0965fcc32fcc00fb90f443
Melanie Sumner <melaniersumner@gmail.com>
2020-03-02 11:37:57 -0600

    Adds new rule: no-duplicate-id
    Update lib/rules/no-duplicate-id.js
    
    Co-Authored-By: Tobias Bieniek <tobias.bieniek@gmail.com>
    
    Update docs/rule/no-duplicate-id.md
    
    Add failing tests: Mustache + Concat
    
    Convert data structure of `already seen` ids: Array -> Set
    
    Add node check function that supports `@id`, `Concat`, `Mustache`
    
    Add support for String-only Mustache + Concats
    
    Abstract Mustache + Concat evaluation to utility functions
    
    Squash / cleanup for adding BlockStatement + elementId support
    
    Restore config
    
    removing errant vscode settings file
    
    Update lib/rules/no-duplicate-id.js
    
    Chore: lint + add rule to README

---
+++ test/unit/rules/no-duplicate-id-test.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const ERROR_MESSAGE = require('../../../lib/rules/no-duplicate-id').ERROR_MESSAGE;
+const generateRuleTests = require('../../helpers/rule-test-harness');
+
+generateRuleTests({
+  name: 'no-duplicate-id',
+
+  config: true,
+
+  good: [
+    // Unique sibling TextNode IDs
+    '<div id="id-00"></div><div id="id-01"></div>',
+
+    // Ignore MustacheStatements, even duplicates
+    '<div id={{this.divId}}></div>',
+    '<div id={{this.divId}}></div><div id={{this.divId}}></div>',
+
+    // Ignore ConcatStatements, even duplicates
+    '<div id="concat-{{this.divId}}"></div>',
+    '<div id="concat-{{this.divId}}"></div><div id="concat-{{this.divId}}"></div>',
+
+    // Mustache and Concat do not conflict/flag with TextNode
+    '<div id={{id-00}}></div><div id="id-00"></div>',
+    '<div id="id-00"></div><div id={{id-00}}></div>',
+    '<div id="concat-{{id-00}}"></div><div id="concat-id-00"></div>',
+    '<div id="concat-id-00"></div><div id="concat-{{id-00}}"></div>',
+
+    // BlockStatement
+    '<div id="id-00"></div>{{#foo elementId="id-01"}}{{/foo}}',
+    '{{#foo elementId="id-01"}}{{/foo}}<div id="id-00"></div>',
+  ],
+
+  bad: [
+    {
+      template: '<div id="id-00"></div><div id="id-00"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 27,
+        source: 'id="id-00"',
+      },
+    },
+    {
+      template: '<div><div id="id-01"></div></div><div><div id="id-01"></div></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 43,
+        source: 'id="id-01"',
+      },
+    },
+
+    {
+      template: '<div id="id-00"></div><div id={{"id-00"}}></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 27,
+        source: 'id={{"id-00"}}',
+      },
+    },
+
+    {
+      template: '<div id={{"id-00"}}></div><div id="id-00"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 31,
+        source: 'id="id-00"',
+      },
+    },
+
+    {
+      template: '<div id="id-00"></div><div id="id-{{"00"}}"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 27,
+        source: 'id="id-{{"00"}}"',
+      },
+    },
+
+    {
+      template: '<div id="id-00"></div><div id="{{"id"}}-00"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 27,
+        source: 'id="{{"id"}}-00"',
+      },
+    },
+
+    // BlockStatement
+    {
+      template: '<div id="id-00"></div>{{#foo elementId="id-00"}}{{/foo}}',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 22,
+        source: '{{#foo elementId="id-00"}}{{/foo}}',
+      },
+    },
+
+    {
+      template: '{{#foo elementId="id-00"}}{{/foo}}<div id="id-00"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 39,
+        source: 'id="id-00"',
+      },
+    },
+
+    {
+      template: '<div id={{"id-00"}}></div>{{#foo elementId="id-00"}}{{/foo}}',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 26,
+        source: '{{#foo elementId="id-00"}}{{/foo}}',
+      },
+    },
+
+    {
+      template: '{{#foo elementId="id-00"}}{{/foo}}<div id={{"id-00"}}></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 39,
+        source: 'id={{"id-00"}}',
+      },
+    },
+
+    {
+      template: '<div id="id-{{"00"}}"></div>{{#foo elementId="id-00"}}{{/foo}}',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 28,
+        source: '{{#foo elementId="id-00"}}{{/foo}}',
+      },
+    },
+
+    {
+      template: '{{#foo elementId="id-00"}}{{/foo}}<div id="id-{{"00"}}"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 39,
+        source: 'id="id-{{"00"}}"',
+      },
+    },
+
+    {
+      template: '{{#foo elementId="id-00"}}{{/foo}}{{#bar elementId="id-00"}}{{/bar}}',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 34,
+        source: '{{#bar elementId="id-00"}}{{/bar}}',
+      },
+    },
+  ],
+});
