98eb09dce06786f182c430616d51c037c7cf95e1
Cyrille David <cyrille@qonto.eu>
2020-04-13 14:47:07 +0200

    Start an acceptance test for editors integration

---
+++ test/acceptance/editors-integration-test.js
@@ -0,0 +1,64 @@
+'use strict';
+const execa = require('execa');
+const Project = require('../helpers/fake-project');
+const setupEnvVar = require('../helpers/setup-env-var');
+
+function setForBasicDebuggerError(project) {
+  project.setConfig({ rules: { 'no-debugger': true } });
+  project.write({ 'template.hbs': '{{debugger}}' });
+}
+
+function run(project, args, options = {}) {
+  options.reject = false;
+  options.cwd = options.cwd || project.path('.');
+  return execa.sync(require.resolve('../../bin/ember-template-lint.js'), args, options);
+}
+
+describe('editors integration', function () {
+  setupEnvVar('GITHUB_ACTIONS', null);
+  setupEnvVar('FORCE_COLOR', '0');
+  setupEnvVar('LC_ALL', 'en_US');
+
+  // Fake project
+  let project;
+  beforeEach(function () {
+    project = Project.defaultSetup();
+    project.chdir();
+  });
+
+  afterEach(async function () {
+    await project.dispose();
+  });
+
+  describe('reading from stdin', function () {
+    it('prints valid JSON strings with error', function () {
+      setForBasicDebuggerError(project);
+
+      let result = run(
+        project,
+        ['--json', '--filename', 'template.hbs', '/dev/stdin', '<', 'template.hbs'],
+        {
+          shell: true,
+        }
+      );
+
+      let expectedOutputData = {};
+      expectedOutputData['template.hbs'] = [
+        {
+          column: 0,
+          line: 1,
+          message: 'Unexpected {{debugger}} usage.',
+          filePath: 'template.hbs',
+          moduleId: 'template',
+          rule: 'no-debugger',
+          severity: 2,
+          source: '{{debugger}}',
+        },
+      ];
+
+      expect(result.exitCode).toEqual(1);
+      expect(JSON.parse(result.stdout)).toEqual(expectedOutputData);
+      expect(result.stderr).toBeFalsy();
+    });
+  });
+});
