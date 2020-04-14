c8e12d2c9928ea89f663a82fa172952a668a3c8b
Cyrille David <cyrille@qonto.eu>
2020-04-14 16:12:26 +0200

    Reproduce globby error with relative paths
    Error is:
    ```
    RangeError: path should be a `path.relative()`d string, but got "../../tmp-28160qlGvn65K8XAe/fake-project/application.hbs"
    
      34 |     globby
      35 |       // `--no-ignore-pattern` results in `ignorePattern === [false]`
    > 36 |       .sync(pattern, ignorePattern[0] === false ? {} : { ignore: ignorePattern, gitignore: true })
         |        ^
      37 |       .filter((filePath) => filePath.slice(-4) === '.hbs')
      38 |       .forEach((filePath) => result.add(filePath));
      39 |   });
    
      at throwError (node_modules/ignore/index.js:337:9)
      at checkPath (node_modules/ignore/index.js:356:12)
      at Ignore._test (node_modules/ignore/index.js:473:5)
      at Ignore.ignores (node_modules/ignore/index.js:512:17)
      at node_modules/globby/gitignore.js:59:22
      at node_modules/globby/index.js:151:21
          at Array.filter (<anonymous>)
      at AsyncFunction.sync (node_modules/globby/index.js:151:4)
      at forEach (bin/ember-template-lint.js:36:8)
          at Array.forEach (<anonymous>)
      at expandFileGlobs (bin/ember-template-lint.js:33:16)
    ```

--- test/unit/bin/expand-file-globs-test.js
+++ test/unit/bin/expand-file-globs-test.js
@@ -39,4 +39,30 @@ describe('expandFileGlobs', function () {
       expect(files.has('application.hbs')).toBe(false);
     });
   });
+
+  describe('relative path', function () {
+    it('resolves a relative path pattern', function () {
+      // To build a relative path from a tmp directory to another, we need the
+      // last two segments (a randomly generated directory name and  the name of
+      // our project: `fake-project`).
+      //
+      // For reference, here is how the node package `tmp` creates file paths:
+      // https://github.com/raszi/node-tmp/blob/b6465b0665b9d7a788460386a1d9b04870d72532/lib/tmp.js#L470
+      //
+      // So, from `project`'s absolute path, we take the last two segments.
+      let projectSegments = project.path().split('/');
+      let randomDirectoryName = projectSegments[projectSegments.length - 2];
+      let projectName = projectSegments[projectSegments.length - 1];
+      let relativePathToTemplate = `../../${randomDirectoryName}/${projectName}/application.hbs`;
+
+      // we want to test `expandFileGlobs` with a relative path
+      // so, let's change process.cwd to another tmp folder
+      new Project('another-fake-project').chdir();
+
+      let files = expandFileGlobs([relativePathToTemplate], []);
+
+      // files is an error thrown by globby
+      expect(files).toBe(true);
+    });
+  });
 });
