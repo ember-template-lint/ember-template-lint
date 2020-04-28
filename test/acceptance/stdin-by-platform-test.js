d9f0f040a16474474240784b21cb0fa9b77df540
Cyrille David <cyrille@qonto.eu>
2020-04-28 17:06:23 +0200

    Consolidate test cases

--- test/acceptance/stdin-by-platform-test.js
+++ test/acceptance/stdin-by-platform-test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const execa = require('execa');
 const Project = require('../helpers/fake-project');
 const setupEnvVar = require('../helpers/setup-env-var');
@@ -31,63 +33,56 @@ describe('ember-template-lint executable', function () {
     await project.dispose();
   });
 
-  if (process.platform === 'win32') {
-    describe('in Windows Command Prompt', function () {
-      describe('command: `node ember-template-lint --filename template.hbs < template.hbs`', function () {
-        it('reports errors to stdout', async function () {
-          let result = await execa(
-            process.execPath,
-            [
-              require.resolve('../../bin/ember-template-lint.js'),
-              '--filename',
-              'template.hbs',
-              '<',
-              'template.hbs',
-            ],
-            { shell: true, reject: false, cwd: project.path('.') }
-          );
+  describe('command: `node ember-template-lint --filename template.hbs < template.hbs`', function () {
+    it('reports errors to stdout', async function () {
+      let result = await execa(
+        process.execPath,
+        [
+          require.resolve('../../bin/ember-template-lint.js'),
+          '--filename',
+          'template.hbs',
+          '<',
+          'template.hbs',
+        ],
+        { shell: true, reject: false, cwd: project.path('.') }
+      );
 
-          expect(result.stdout).toMatchInlineSnapshot(`
+      expect(result.stdout).toMatchInlineSnapshot(`
             "template.hbs
               1:4  error  Non-translated string used  no-bare-strings
               1:25  error  Non-translated string used  no-bare-strings
 
             ✖ 2 problems (2 errors, 0 warnings)"
           `);
-          expect(result.stderr).toBeFalsy();
-        });
-      });
+      expect(result.stderr).toBeFalsy();
     });
-  }
 
-  if (process.platform === 'linux' || process.platform === 'darwin') {
-    describe('in unix bash', function () {
-      describe('command: `cat template.hbs | ember-template-lint --filename template.hbs`', function () {
-        it('has exit code 1 and reports errors to stdout', async function () {
-          let result = await execa(
-            'cat',
-            [
-              'template.hbs',
-              '|',
-              require.resolve('../../bin/ember-template-lint.js'),
-              '--filename',
-              'template.hbs',
-            ],
-            { shell: true, reject: false, cwd: project.path('.') }
-          );
+    it('has exit code 1 and reports errors to stdout', async function () {
+      let result = await execa(
+        process.execPath,
+        [require.resolve('../../bin/ember-template-lint.js'), '--filename', 'template.hbs'],
+        {
+          shell: false,
+          reject: false,
+          cwd: project.path('.'),
+          input: fs.readFileSync(path.resolve('template.hbs')),
+        }
+      );
 
-          expect(result.exitCode).toEqual(1);
-          expect(result.stdout).toMatchInlineSnapshot(`
+      expect(result.exitCode).toEqual(1);
+      expect(result.stdout).toMatchInlineSnapshot(`
             "template.hbs
               1:4  error  Non-translated string used  no-bare-strings
               1:25  error  Non-translated string used  no-bare-strings
 
             ✖ 2 problems (2 errors, 0 warnings)"
           `);
-          expect(result.stderr).toBeFalsy();
-        });
-      });
+      expect(result.stderr).toBeFalsy();
+    });
+  });
 
+  if (process.platform !== 'win32') {
+    describe('posix environments', function () {
       describe('command: `cat template.hbs | ember-template-lint --filename template.hbs -`', function () {
         it('has exit code 1 and reports errors to stdout', async function () {
           let result = await execa(
@@ -100,26 +95,6 @@ describe('ember-template-lint executable', function () {
               'template.hbs',
               '-',
             ],
-            { shell: true, reject: false, cwd: project.path('.') }
-          );
-
-          expect(result.exitCode).toEqual(1);
-          expect(result.stdout).toMatchInlineSnapshot(`
-            "template.hbs
-              1:4  error  Non-translated string used  no-bare-strings
-              1:25  error  Non-translated string used  no-bare-strings
-
-            ✖ 2 problems (2 errors, 0 warnings)"
-          `);
-          expect(result.stderr).toBeFalsy();
-        });
-      });
-
-      describe('command: `ember-template-lint --filename template.hbs < template.hbs`', function () {
-        it('has exit code 1 and reports errors to stdout', async function () {
-          let result = await execa(
-            require.resolve('../../bin/ember-template-lint.js'),
-            ['--filename', 'template.hbs', '<', 'template.hbs'],
             { shell: true, reject: false, cwd: project.path('.') }
           );
 
