f4b7bf0a27e6048ae31b0e75ef8332a7db3b47c2
Michael Peirce <msp700@gmail.com>
2020-06-28 11:51:34 -0700

    Rename flatten to flat

--- test/unit/utils/javascript-test.js
+++ test/unit/utils/javascript-test.js
@@ -2,19 +2,19 @@
 
 const javascriptUtils = require('../../../lib/utils/javascript');
 
-describe('flatten', function () {
+describe('flat', function () {
   it('correctly flattens arrays', function () {
-    expect(javascriptUtils.flatten([])).toStrictEqual([]);
-    expect(javascriptUtils.flatten(null)).toBeNull();
-    expect(javascriptUtils.flatten([[1], [2]])).toStrictEqual([1, 2]);
+    expect(javascriptUtils.flat([])).toStrictEqual([]);
+    expect(javascriptUtils.flat(null)).toBeNull();
+    expect(javascriptUtils.flat([[1], [2]])).toStrictEqual([1, 2]);
     expect(
-      javascriptUtils.flatten([
+      javascriptUtils.flat([
         [1, 2],
         [3, 4],
       ])
     ).toStrictEqual([1, 2, 3, 4]);
-    expect(javascriptUtils.flatten([[1], [2, 3], [4, 5, 6]])).toStrictEqual([1, 2, 3, 4, 5, 6]); // different lengths forward
-    expect(javascriptUtils.flatten([[6, 5, 4], [3, 2], [1]])).toStrictEqual([6, 5, 4, 3, 2, 1]); // different lengths reverse
-    expect(javascriptUtils.flatten([[1], null, [2]])).toStrictEqual([1, null, 2]); // with null array
+    expect(javascriptUtils.flat([[1], [2, 3], [4, 5, 6]])).toStrictEqual([1, 2, 3, 4, 5, 6]); // different lengths forward
+    expect(javascriptUtils.flat([[6, 5, 4], [3, 2], [1]])).toStrictEqual([6, 5, 4, 3, 2, 1]); // different lengths reverse
+    expect(javascriptUtils.flat([[1], null, [2]])).toStrictEqual([1, null, 2]); // with null array
   });
 });
