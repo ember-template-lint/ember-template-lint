d68c1528c2c28f2d36115997159db2604b1693b6
Michael Peirce <msp700@gmail.com>
2020-06-28 11:47:24 -0700

    Remove extra utils

--- test/unit/utils/javascript-test.js
+++ test/unit/utils/javascript-test.js
@@ -2,21 +2,6 @@
 
 const javascriptUtils = require('../../../lib/utils/javascript');
 
-describe('duplicateArrays', () => {
-  it('returns the right result', () => {
-    expect(javascriptUtils.duplicateArrays([], 2)).toStrictEqual([]);
-    expect(javascriptUtils.duplicateArrays(['abc'], 2)).toStrictEqual(['abc', 'abc', 'abc']);
-    expect(javascriptUtils.duplicateArrays(['first', 'second'], 2)).toStrictEqual([
-      'first',
-      'second',
-      'first',
-      'second',
-      'first',
-      'second',
-    ]);
-  });
-});
-
 describe('flatten', function () {
   it('correctly flattens arrays', function () {
     expect(javascriptUtils.flatten([])).toStrictEqual([]);
@@ -31,13 +16,5 @@ describe('flatten', function () {
     expect(javascriptUtils.flatten([[1], [2, 3], [4, 5, 6]])).toStrictEqual([1, 2, 3, 4, 5, 6]); // different lengths forward
     expect(javascriptUtils.flatten([[6, 5, 4], [3, 2], [1]])).toStrictEqual([6, 5, 4, 3, 2, 1]); // different lengths reverse
     expect(javascriptUtils.flatten([[1], null, [2]])).toStrictEqual([1, null, 2]); // with null array
-  });
-});
-
-describe('removeWhitespace', () => {
-  it('returns the right result', () => {
-    expect(javascriptUtils.removeWhitespace('abcdef')).toStrictEqual('abcdef');
-    expect(javascriptUtils.removeWhitespace('abc def')).toStrictEqual('abcdef');
-    expect(javascriptUtils.removeWhitespace(' abc def ')).toStrictEqual('abcdef');
   });
 });
