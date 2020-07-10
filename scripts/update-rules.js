ac163322423f8e23f052cdad08477870e23b199e
Cyrille David <cyrille@qonto.eu>
2020-07-10 11:21:34 +0200

    Update `yarn update` to add :wrench: emoji
    If the rule has a fixer.

--- scripts/update-rules.js
+++ scripts/update-rules.js
@@ -2,6 +2,10 @@
 
 const fs = require('fs');
 const path = require('path');
+
+const { parse } = require('@babel/parser');
+const { default: traverse } = require('@babel/traverse');
+
 const rules = require('../lib/rules');
 const { rules: recommendedRules } = require('../lib/config/recommended');
 const { rules: octaneRules } = require('../lib/config/octane');
@@ -15,6 +19,29 @@ const tablePlaceholder = /<!--RULES_TABLE_START-->[\S\s]*<!--RULES_TABLE_END-->/
 const EMOJI_STAR = ':white_check_mark:';
 const EMOJI_OCTANE = ':car:';
 const EMOJI_STYLISTIC = ':dress:';
+const EMOJI_FIXABLE = ':wrench:';
+
+function isRuleFixable(ruleName) {
+  const relativePath = ruleName.startsWith('deprecated-')
+    ? `../lib/rules/deprecations/${ruleName}.js`
+    : `../lib/rules/${ruleName}.js`;
+  const pathRule = path.resolve(__dirname, relativePath);
+  let rule = fs.readFileSync(pathRule, { encoding: 'utf8' });
+
+  let ast = parse(rule, { sourceType: 'module' });
+
+  let isFixable = false;
+
+  traverse(ast, {
+    ObjectProperty(path) {
+      if (path.node.key.name === 'isFixable' && path.node.value.value !== false) {
+        isFixable = true;
+      }
+    },
+  });
+
+  return isFixable;
+}
 
 // Generate rule table contents.
 const rulesTableContent = Object.keys(rules)
@@ -24,10 +51,13 @@ const rulesTableContent = Object.keys(rules)
     const isRecommended = Object.prototype.hasOwnProperty.call(recommendedRules, ruleName);
     const isOctane = Object.prototype.hasOwnProperty.call(octaneRules, ruleName);
     const isStylistic = Object.prototype.hasOwnProperty.call(stylisticRules, ruleName);
+    const isFixable = isRuleFixable(ruleName);
+
     const emoji = [
       isRecommended ? EMOJI_STAR : '',
       isOctane ? EMOJI_OCTANE : '',
       isStylistic ? EMOJI_STYLISTIC : '',
+      isFixable ? EMOJI_FIXABLE : '',
     ].join('');
 
     const url = ruleName.startsWith('deprecated-')
