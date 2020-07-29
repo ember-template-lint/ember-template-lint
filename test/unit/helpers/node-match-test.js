02acc151e754058e69596537292bedb8269bef23
Joseph D. Sumner <me@jds.dev>
2020-07-29 16:24:51 -0500

    Chore: naming convention

--- test/unit/helpers/node-match-test.js
+++ test/unit/helpers/node-match-test.js
@@ -1,47 +1,47 @@
 'use strict';
 
-const nodeMatchesRef = require('../../../lib/helpers/node-match').nodeMatchesRef;
-const nodeMatchesValidRef = require('../../../lib/helpers/node-match').nodeMatchesValidRef;
+const nodeMatchesRefNode = require('../../../lib/helpers/node-match').nodeMatchesRef;
+const nodeMatchesValidRefNode = require('../../../lib/helpers/node-match').nodeMatchesValidRef;
 
-describe('Single Node-Ref Matches', function () {
-  it('Exact Matches', function () {
-    let test = { a: 1, b: { c: 3, d: 4 } };
-    let ref = { a: 1, b: { c: 3, d: 4 } };
-    let res = nodeMatchesRef(test, ref);
-    expect(res).toBe(true);
+describe('testNode matched against single refNode', function () {
+  it('Exact Match', function () {
+    let testNode = { a: 1, b: { c: 3, d: 4 } };
+    let refNode = { a: 1, b: { c: 3, d: 4 } };
+    let result = nodeMatchesRefNode(testNode, refNode);
+    expect(result).toBe(true);
   });
 
   it('testNode match (k+v) with excess', function () {
-    let test = { a: 1, b: { c: 3, d: 4 }, d: 5 };
-    let ref = { a: 1, b: { c: 3, d: 4 } };
-    let res = nodeMatchesRef(test, ref);
-    expect(res).toBe(true);
+    let testNode = { a: 1, b: { c: 3, d: 4 }, d: 5 };
+    let refNode = { a: 1, b: { c: 3, d: 4 } };
+    let result = nodeMatchesRefNode(testNode, refNode);
+    expect(result).toBe(true);
   });
 
   it('testNode match (k only)', function () {
-    let test = { a: 2, b: { c: 3, d: 4 } };
-    let ref = { a: 1, b: { c: 3, d: 4 } };
-    let res = nodeMatchesRef(test, ref);
-    expect(res).toBe(false);
+    let testNode = { a: 2, b: { c: 3, d: 4 } };
+    let refNode = { a: 1, b: { c: 3, d: 4 } };
+    let result = nodeMatchesRefNode(testNode, refNode);
+    expect(result).toBe(false);
   });
 
   it('testNode non-match (-k)', function () {
-    let test = { a: 2, b: { c: 3 } };
-    let ref = { a: 1, b: { c: 3, d: 4 } };
-    let res = nodeMatchesRef(test, ref);
-    expect(res).toBe(false);
+    let testNode = { a: 2, b: { c: 3 } };
+    let refNode = { a: 1, b: { c: 3, d: 4 } };
+    let result = nodeMatchesRefNode(testNode, refNode);
+    expect(result).toBe(false);
   });
 });
 
 describe('Multiple Valid Refs Possible', function () {
   it('Exact Match', function () {
-    let test = { a: 1, b: { c: 3, d: 4 } };
-    let refs = [
+    let testNode = { a: 1, b: { c: 3, d: 4 } };
+    let refNodes = [
       { a: 1, b: { c: 3, d: 4 } },
       { a: 2, b: { c: 3, d: 6 } },
       { a: 'dog', b: { c: 3, d: 7 } },
     ];
-    let res = nodeMatchesValidRef(test, refs);
-    expect(res).toBe(true);
+    let result = nodeMatchesValidRefNode(testNode, refNodes);
+    expect(result).toBe(true);
   });
 });
