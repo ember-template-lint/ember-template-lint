2c4f592d0495f4849665ce587ffc5d8c02bfbfb2
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2020-10-01 00:56:40 +0000

    add <textarea> support to require-input-label
    A <textarea> component is still an input, and the same
    labelling WCAG rules apply.

--- test/unit/rules/require-input-label-test.js
+++ test/unit/rules/require-input-label-test.js
@@ -29,10 +29,22 @@ generateRuleTests({
     '<label>Text here {{input}}</label>',
     '<input id="label-input" ...attributes>',
 
-    // Hidden inputs are allowed.
-    '<input type="hidden"/>',
-    '<Input type="hidden" />',
-    '{{input type="hidden"}}',
+    // Same logic applies to textareas
+    '<label>LabelText<textarea /></label>',
+    '<label><textarea />LabelText</label>',
+    '<label>LabelText<Textarea /></label>',
+    '<label><Textarea />LabelText</label>',
+    '<label>Label Text<div><textarea /></div></label>', // technically okay, hopefully no one does this though
+    '<textarea id="probablyHasLabel" />', // it's likely to have an associated label if it has an id attribute
+    '<textarea aria-label={{labelText}} />',
+    '<textarea aria-labelledby="someIdValue" />',
+    '<textarea ...attributes/>', // we are unable to correctly determine if this has a label or not, so we have to allow it
+    '<Textarea ...attributes />',
+    '<Textarea id="foo" />',
+    '{{textarea id="foo"}}',
+    '<label>Text here<Textarea /></label>',
+    '<label>Text here {{textarea}}</label>',
+    '<textarea id="label-input" ...attributes />',
   ],
 
   bad: [
@@ -118,57 +130,84 @@ generateRuleTests({
       },
     },
     {
-      template: '{{input type="button"}}',
+      template: '<div><textarea /></div>',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
-        column: 0,
-        source: '{{input type="button"}}',
+        column: 5,
+        source: '<textarea />',
       },
     },
     {
-      template: '{{input type=myType}}',
+      template: '<textarea />',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 0,
-        source: '{{input type=myType}}',
+        source: '<textarea />',
       },
     },
     {
-      template: '<input type="button"/>',
+      template: '<textarea title="some title value" />',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 0,
-        source: '<input type="button"/>',
+        source: '<textarea title="some title value" />',
       },
     },
     {
-      template: '<input type={{myType}}/>',
+      template: '<label><textarea /></label>',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
-        column: 0,
-        source: '<input type={{myType}}/>',
+        column: 7,
+        source: '<textarea />',
       },
     },
     {
-      template: '<Input type="button"/>',
+      template: '<div>{{textarea}}</div>',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
-        column: 0,
-        source: '<Input type="button"/>',
+        column: 5,
+        source: '{{textarea}}',
       },
     },
     {
-      template: '<Input type={{myType}}/>',
+      template: '<Textarea />',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 0,
-        source: '<Input type={{myType}}/>',
+        source: '<Textarea />',
+      },
+    },
+    {
+      template: '<textarea aria-label="first label" aria-labelledby="second label" />',
+      result: {
+        message: ERROR_MESSAGE_MULTIPLE_LABEL,
+        line: 1,
+        column: 0,
+        source: '<textarea aria-label="first label" aria-labelledby="second label" />',
+      },
+    },
+    {
+      template: '<textarea id="label-input" aria-label="second label" />',
+      result: {
+        message: ERROR_MESSAGE_MULTIPLE_LABEL,
+        line: 1,
+        column: 0,
+        source: '<textarea id="label-input" aria-label="second label" />',
+      },
+    },
+    {
+      template: '<label>Textarea label<textarea aria-label="Custom label" /></label>',
+      result: {
+        message: ERROR_MESSAGE_MULTIPLE_LABEL,
+        line: 1,
+        column: 21,
+        source: '<textarea aria-label="Custom label" />',
       },
     },
   ],
