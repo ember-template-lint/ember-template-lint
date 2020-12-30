7c46d0636b14dd71fe0d558af7154eb90fa009cf
Derek Wickern <dwickern@gmail.com>
2020-12-30 09:56:32 -0800

    add rule: no-this-in-template-only-components

---
+++ docs/rule/no-this-in-template-only-components.md
@@ -0,0 +1,44 @@
+# no-this-in-template-only-components
+
+There is no `this` context in template-only components.
+
+:wrench: The `--fix` option on the command line can automatically fix some of the problems reported by this rule.
+
+## Examples
+
+This rule **forbids** `this` if the template has no corresponding `component.js`:
+
+```hbs
+<h1>Hello {{this.name}}!</h1>
+```
+
+This rule **allows** `this` if the template has a corresponding `component.js`:
+
+```js
+import Component from '@ember/component';
+
+export default Component.extend({
+  name: 'Derek'
+});
+```
+
+```hbs
+<h1>Hello {{this.name}}!</h1>
+```
+
+The `--fix` option will convert to named arguments:
+
+```hbs
+<h1>Hello {{@name}}!</h1>
+```
+
+## Migration
+
+* use [ember-no-implicit-this-codemod](https://github.com/ember-codemods/ember-no-implicit-this-codemod)
+* [upgrade to Glimmer components](https://guides.emberjs.com/release/upgrading/current-edition/glimmer-components/), which don't allow ambiguous access
+  * classic components have [auto-reflection](https://github.com/emberjs/rfcs/blob/master/text/0276-named-args.md#motivation), and can use `this.myArgName` or `this.args.myArgNme` or `@myArgName` interchangeably
+
+## References
+
+* [Glimmer components](https://guides.emberjs.com/release/upgrading/current-edition/glimmer-components/)
+* [rfcs/named args](https://github.com/emberjs/rfcs/blob/master/text/0276-named-args.md#motivation)
---
+++ test/unit/rules/no-this-in-template-only-components-test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const { message } = require('../../../lib/rules/no-this-in-template-only-components');
+const generateRuleTests = require('../../helpers/rule-test-harness');
+
+generateRuleTests({
+  name: 'no-this-in-template-only-components',
+
+  config: true,
+
+  good: [
+    '{{welcome-page}}',
+    '<WelcomePage />',
+    '<MyComponent @prop={{can "edit" @model}} />',
+    '{{my-component model=model}}',
+  ],
+
+  bad: [
+    {
+      template: '{{my-component model=this.model}}',
+      fixedTemplate: '{{my-component model=@model}}',
+
+      result: {
+        message: message('this.model', '@model'),
+        source: 'this.model',
+        line: 1,
+        column: 21,
+        isFixable: true,
+      },
+    },
+    {
+      template: '{{my-component action=(action this.myAction)}}',
+      fixedTemplate: '{{my-component action=(action @myAction)}}',
+
+      result: {
+        message: message('this.myAction', '@myAction'),
+        source: 'this.myAction',
+        line: 1,
+        column: 30,
+        isFixable: true,
+      },
+    },
+    {
+      template: '<MyComponent @prop={{can "edit" this.model}} />',
+      fixedTemplate: '<MyComponent @prop={{can "edit" @model}} />',
+
+      result: {
+        message: message('this.model', '@model'),
+        source: 'this.model',
+        line: 1,
+        column: 32,
+        isFixable: true,
+      },
+    },
+    {
+      template: '{{input id=(concat this.elementId "-username")}}',
+      result: {
+        message: message('this.elementId', '@elementId'),
+        source: 'this.elementId',
+        line: 1,
+        column: 19,
+        isFixable: false,
+      },
+    },
+  ],
+});
