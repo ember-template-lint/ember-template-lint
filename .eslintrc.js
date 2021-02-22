8663696a55296bd73df310387817ab21599abb69
Robert Jackson <rjackson@linkedin.com>
2021-02-22 11:34:25 -0500

    Remove prettier/unicorn reference from .eslintrc.js
    It is no longer needed (eslint-config-prettier@8 automatically includes
    `prettier/unicorn`).

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,7 @@ module.exports = {
     'plugin:import/warnings',
     'plugin:node/recommended',
     'plugin:unicorn/recommended',
-    'prettier',
-    'prettier/unicorn',
+    'plugin:prettier/recommended',
   ],
   rules: {
     // Optional eslint rules:
@@ -113,8 +112,6 @@ module.exports = {
         alphabetize: { order: 'asc', ignoreCase: true },
       },
     ],
-
-    'prettier/prettier': 'error',
 
     // Unicorn rules:
     'unicorn/consistent-destructuring': 'off',
