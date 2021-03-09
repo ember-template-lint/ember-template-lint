7eb72cfb36d04b0f423aced0df9ae6e8c6f61b2b
Steve Calvert <scalvert@linkedin.com>
2021-03-08 16:42:29 -0800

    Removing resolutions in package.json. Renaming shadowed variable

--- dev/generate.js
+++ dev/generate.js
@@ -3,14 +3,14 @@ const { createEnv } = require('yeoman-environment');
 const AVAILABLE_GENERATORS = ['new-rule', 'rules-index'];
 
 async function main() {
-  let generator = process.argv.slice(2);
+  let generatorName = process.argv.slice(2);
   let env = createEnv();
 
   for (const generator of AVAILABLE_GENERATORS) {
     env.register(require.resolve(`./${generator}-generator`), `ember-template-lint:${generator}`);
   }
 
-  await env.run(`ember-template-lint:${generator}`);
+  await env.run(`ember-template-lint:${generatorName}`);
 }
 
 main();
