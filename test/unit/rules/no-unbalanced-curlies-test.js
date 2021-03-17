b28d2c44aeb0b2cfa6a714f4dd1a8ea1ac3dd1f6
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-03-17 14:48:19 +0300

    more good test cases

--- test/unit/rules/no-unbalanced-curlies-test.js
+++ test/unit/rules/no-unbalanced-curlies-test.js
@@ -8,7 +8,15 @@ generateRuleTests({
 
   config: true,
 
-  good: ['{foo}', '{{foo}}', '{{{foo}}}', '{{{foo\n}}}', '\\{{foo}}'],
+  good: [
+    '{foo}',
+    '{{foo}}',
+    '{{{foo}}}',
+    '{{{foo\n}}}',
+    '\\{{foo}}',
+    '\\{{foo}}\\{{foo}}',
+    '\\{{foo}}{{foo}}',
+  ],
 
   bad: [
     {
