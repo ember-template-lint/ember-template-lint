bb122c56cca701324c6dc835f13eb2aaddf1ddf3
Steve Calvert <scalvert@linkedin.com>
2021-05-24 10:11:51 -0700

    Conditionally running generator tests

--- test/unit/generator-test.js
+++ test/unit/generator-test.js
@@ -10,35 +10,37 @@ const Project = require('../helpers/fake-project');
 
 const ROOT = process.cwd();
 
-describe('generators', () => {
-  let project = null;
+if (Number(process.versions.node.split('.')[0]) > 10) {
+  describe('generators', () => {
+    let project = null;
 
-  beforeEach(function () {
-    project = new Project();
-  });
-
-  afterEach(async function () {
-    process.chdir(ROOT);
-    await project.dispose();
-  });
-
-  it('generates the rules index file correctly', async function () {
-    jest.setTimeout(10000);
-    project.writeSync();
-
-    let rulesIndexPath = path.resolve(__dirname, '../../lib/rules');
-    let expectedRulesIndex = fs.readFileSync(path.join(rulesIndexPath, 'index.js'), {
-      encoding: 'utf-8',
+    beforeEach(function () {
+      project = new Project();
     });
 
-    await helpers.run(RulesIndexGenerator).cd(project.baseDir).withOptions({
-      rulesIndexPath,
+    afterEach(async function () {
+      process.chdir(ROOT);
+      await project.dispose();
     });
 
-    let generatedRulesIndex = fs.readFileSync(path.join(project.baseDir, 'lib/rules/index.js'), {
-      encoding: 'utf-8',
-    });
+    it('generates the rules index file correctly', async function () {
+      jest.setTimeout(10000);
+      project.writeSync();
 
-    expect(generatedRulesIndex).toEqual(expectedRulesIndex);
+      let rulesIndexPath = path.resolve(__dirname, '../../lib/rules');
+      let expectedRulesIndex = fs.readFileSync(path.join(rulesIndexPath, 'index.js'), {
+        encoding: 'utf-8',
+      });
+
+      await helpers.run(RulesIndexGenerator).cd(project.baseDir).withOptions({
+        rulesIndexPath,
+      });
+
+      let generatedRulesIndex = fs.readFileSync(path.join(project.baseDir, 'lib/rules/index.js'), {
+        encoding: 'utf-8',
+      });
+
+      expect(generatedRulesIndex).toEqual(expectedRulesIndex);
+    });
   });
-});
+}
