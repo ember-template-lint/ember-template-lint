ca279ce4b99f00628bd3f302d4ccf87a239551e7
Jacob Durham <jfdurham88@gmail.com>
2021-08-05 10:46:49 -0600

    refactors `no-down-event-binding` to `no-pointer-down-event-binding`
    introduced recommended rule `no-down-event-binding` references [WCAG failure of success criteria](https://www.w3.org/WAI/WCAG21/Techniques/failures/F101) for `down` events that only pertain to pointer-down events. current implementation of `no-down-event-binding` also applies to `keydown` events, which does not fit the intention of the WCAG docs, and therefore should not be included in this lint rule.
    
    - updates related file names to reflect `no-pointer-down-event-binding`
    - updates node check for inclusion in "mousedown"|"pointerdown"
    - updates related doc and test files as appropriate

--- test/unit/rules/no-pointer-down-event-binding-test.js
+++ test/unit/rules/no-pointer-down-event-binding-test.js
@@ -1,61 +1,61 @@
 'use strict';
 
-const { ERROR_MESSAGE } = require('../../../lib/rules/no-down-event-binding');
+const { ERROR_MESSAGE } = require('../../../lib/rules/no-pointer-down-event-binding');
 const generateRuleTests = require('../../helpers/rule-test-harness');
 
 generateRuleTests({
-  name: 'no-down-event-binding',
+  name: 'no-pointer-down-event-binding',
 
   config: true,
 
   good: [
     // Typical event binding
-    "<div {{on 'keyup' this.doSomething}}></div>",
-    "<div {{action this.doSomething on='keyup'}}></div>",
+    "<div {{on 'mouseup' this.doSomething}}></div>",
+    "<div {{action this.doSomething on='mouseup'}}></div>",
     // DOM event handling through attributes
-    '<input type="text" onkeyup="myFunction()">',
+    '<input type="text" onmouseup="myFunction()">',
     // For now, we're not catching component arguments
-    '{{my-component keyDown=this.doSomething}}',
-    '<MyComponent @keyDown={{this.doSomething}} />',
+    '{{my-component mouseDown=this.doSomething}}',
+    '<MyComponent @mouseDown={{this.doSomething}} />',
   ],
 
   bad: [
     {
-      template: "<div {{on 'keydown' this.doSomething}}></div>",
+      template: "<div {{on 'mousedown' this.doSomething}}></div>",
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 10,
-        source: "'keydown'",
+        source: "'mousedown'",
       },
     },
     {
-      template: "<div {{action this.doSomething on='keydown'}}></div>",
+      template: "<div {{action this.doSomething on='mousedown'}}></div>",
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 34,
-        source: "'keydown'",
+        source: "'mousedown'",
       },
     },
     {
       // Detecting the `on` param works, even if it's not the first hash param to `{{action}}`
-      template: "<div {{action this.doSomething preventDefault=true on='keydown'}}></div>",
+      template: "<div {{action this.doSomething preventDefault=true on='mousedown'}}></div>",
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 54,
-        source: "'keydown'",
+        source: "'mousedown'",
       },
     },
     {
       // DOM event handling through attributes
-      template: '<input type="text" onkeydown="myFunction()">',
+      template: '<input type="text" onmousedown="myFunction()">',
       result: {
         message: ERROR_MESSAGE,
         line: 1,
         column: 19,
-        source: 'onkeydown="myFunction()"',
+        source: 'onmousedown="myFunction()"',
       },
     },
   ],
