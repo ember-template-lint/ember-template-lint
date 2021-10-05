5603838b54add807896aea08b7d1fbc856da58d1
Glenn Cueto <gcueto@linkedin.com>
2021-10-05 14:55:11 -0700

    implement no empty headings rule

---
+++ test/unit/rules/no-empty-headings-test.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const { ERROR_MESSAGE } = require('../../../lib/rules/no-empty-headings');
+const generateRuleTests = require('../../helpers/rule-test-harness');
+
+generateRuleTests({
+  name: 'no-empty-headings',
+
+  config: true,
+
+  good: [
+    '<h1>Accessible Heading</h1>',
+    '<h1>Accessible&nbsp;Heading</h1>',
+    '<h1 aria-hidden="true">Valid Heading</h1>',
+    '<h1 aria-hidden="true"><span>Valid Heading</span></h1>',
+    '<h1 aria-hidden="false">Accessible Heading</h1>',
+    '<h1 hidden>Valid Heading</h1>',
+    '<h1 hidden><span>Valid Heading</span></h1>',
+    '<h1><span aria-hidden="true">Hidden text</span><span>Visible text</span></h1>',
+    '<h1><span aria-hidden="true">Hidden text</span>Visible text</h1>',
+    '<div role="heading">Accessible Text</div>',
+    '<div role="heading"><span>Accessible Text</span></div>',
+    '<div role="heading"><span aria-hidden="true">Hidden text</span><span>Visible text</span></div>',
+    '<div role="heading"><span aria-hidden="true">Hidden text</span>Visible text</div>',
+  ],
+
+  bad: [
+    {
+      template: '<h1></h1>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<h1></h1>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<h1> \n &nbsp;</h1>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<h1> \n &nbsp;</h1>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<h1> &nbsp; <div aria-hidden="true">Some hidden text</div></h1>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<h1> &nbsp; <div aria-hidden="true">Some hidden text</div></h1>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<h1><span aria-hidden="true">Inaccessible text</span></h1>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<h1><span aria-hidden="true">Inaccessible text</span></h1>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<h1><span hidden>Inaccessible text</span></h1>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<h1><span hidden>Inaccessible text</span></h1>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template:
+        '<h1><span aria-hidden="true">Hidden text</span><span aria-hidden="true">Hidden text</span></h1>',
+      result: {
+        message: ERROR_MESSAGE,
+        source:
+          '<h1><span aria-hidden="true">Hidden text</span><span aria-hidden="true">Hidden text</span></h1>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<div role="heading"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<div role="heading"></div>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<div role="heading"><span aria-hidden="true">Inaccessible text</span></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<div role="heading"><span aria-hidden="true">Inaccessible text</span></div>',
+        line: 1,
+        column: 0,
+      },
+    },
+    {
+      template: '<div role="heading"><span hidden>Inaccessible text</span></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        source: '<div role="heading"><span hidden>Inaccessible text</span></div>',
+        line: 1,
+        column: 0,
+      },
+    },
+  ],
+});
