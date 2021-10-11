c1233076f7cf22498d67549f99dee3e512d94278
Glenn Cueto <gcueto@linkedin.com>
2021-10-11 14:57:23 -0700

    use allowEmptyLinks rule option

--- test/unit/rules/no-invalid-link-text-test.js
+++ test/unit/rules/no-invalid-link-text-test.js
@@ -2,6 +2,10 @@
 
 const generateRuleTests = require('../../helpers/rule-test-harness');
 
+const allowEmptyLinksConfigFalse = {
+  allowEmptyLinks: false,
+};
+
 generateRuleTests({
   name: 'no-invalid-link-text',
 
@@ -12,10 +16,23 @@ generateRuleTests({
     '{{#link-to}} click here to read more about our company{{/link-to}}',
     '<LinkTo>Read more about ways semantic HTML can make your code more accessible.</LinkTo>',
     '<LinkTo>{{foo}} more</LinkTo>',
-    '<a href="https://myurl.com" aria-labelledby="some-id"></a>',
-    '<a href="https://myurl.com" aria-label="click here to read about our company"></a>',
-    '<a href="https://myurl.com" aria-hidden="true"></a>',
-    '<a href="https://myurl.com" hidden></a>',
+    {
+      config: allowEmptyLinksConfigFalse,
+      template: '<a href="https://myurl.com" aria-labelledby="some-id"></a>',
+    },
+    {
+      config: allowEmptyLinksConfigFalse,
+      template:
+        '<a href="https://myurl.com" aria-label="click here to read about our company"></a>',
+    },
+    {
+      config: allowEmptyLinksConfigFalse,
+      template: '<a href="https://myurl.com" aria-hidden="true"></a>',
+    },
+    {
+      config: allowEmptyLinksConfigFalse,
+      template: '<a href="https://myurl.com" hidden></a>',
+    },
   ],
 
   bad: [
@@ -47,6 +64,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<a href="https://myurl.com"></a>',
       result: {
         message: 'Links should have descriptive text',
@@ -56,6 +74,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<a href="https://myurl.com"> </a>',
       result: {
         message: 'Links should have descriptive text',
@@ -65,6 +84,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<a href="https://myurl.com"> &nbsp; \n</a>',
       result: {
         message: 'Links should have descriptive text',
@@ -74,6 +94,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<a aria-labelledby="" href="https://myurl.com">Click here</a>',
       result: {
         message: 'Links should have descriptive text',
@@ -83,6 +104,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<a aria-labelledby=" " href="https://myurl.com">Click here</a>',
       result: {
         message: 'Links should have descriptive text',
@@ -92,6 +114,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<a aria-label="Click here" href="https://myurl.com">Click here</a>',
       result: {
         message: 'Links should have descriptive text',
@@ -101,6 +124,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<LinkTo></LinkTo>',
       result: {
         message: 'Links should have descriptive text',
@@ -110,6 +134,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '<LinkTo> &nbsp; \n</LinkTo>',
       result: {
         message: 'Links should have descriptive text',
@@ -119,6 +144,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '{{#link-to}}{{/link-to}}',
       result: {
         message: 'Links should have descriptive text',
@@ -128,6 +154,7 @@ generateRuleTests({
       },
     },
     {
+      config: allowEmptyLinksConfigFalse,
       template: '{{#link-to}} &nbsp; \n{{/link-to}}',
       result: {
         message: 'Links should have descriptive text',
