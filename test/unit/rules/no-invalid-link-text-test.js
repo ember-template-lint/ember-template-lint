5bf353e1927cf0af7cf979c1cfc44804682f1c98
Glenn Cueto <gcueto@linkedin.com>
2021-10-12 12:12:05 -0700

    test for default true config value

--- test/unit/rules/no-invalid-link-text-test.js
+++ test/unit/rules/no-invalid-link-text-test.js
@@ -16,6 +16,11 @@ generateRuleTests({
     '{{#link-to}} click here to read more about our company{{/link-to}}',
     '<LinkTo>Read more about ways semantic HTML can make your code more accessible.</LinkTo>',
     '<LinkTo>{{foo}} more</LinkTo>',
+    '<a href="https://myurl.com" aria-labelledby="some-id"></a>',
+    '<a href="https://myurl.com" aria-label="click here to read about our company"></a>',
+    '<a href="https://myurl.com" aria-hidden="true"></a>',
+    '<a href="https://myurl.com" hidden></a>',
+    '<a href="https://myurl.com"></a>',
     {
       config: allowEmptyLinksConfigFalse,
       template: '<a href="https://myurl.com" aria-labelledby="some-id"></a>',
