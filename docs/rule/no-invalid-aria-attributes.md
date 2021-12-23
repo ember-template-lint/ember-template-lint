729fafe7869fd021a3e777b140b8c5e2a95292c0
Judith Lung <judithhin@gmail.com>
2021-12-23 15:21:40 -0800

    Added aria-attribute validation

--- docs/rule/no-invalid-aria-attributes.md
+++ docs/rule/no-invalid-aria-attributes.md
@@ -1,6 +1,6 @@
 # no-invalid-aria-attributes
 
-ARIA attributes are used to provide an element with specific accessibility functions. An ARIA attribute is invalid if it is either misspelled or does not currently exist in the [WAI-ARIA States and Properties spec](https://www.w3.org/WAI/PF/aria-1.1/states_and_properties). An invalid ARIA attribute performs no function, and the element it belongs to remains inaccessible.
+ARIA attributes are used to provide an element with specific accessibility functions. An ARIA attribute is invalid if its name or values are either misspelled or do not currently exist in the [WAI-ARIA States and Properties spec](https://www.w3.org/WAI/PF/aria-1.1/states_and_properties).
 
 This rule disallows the use of invalid ARIA attributes.
 
@@ -10,12 +10,16 @@ This rule **forbids** the following:
 
 ```hbs
 <input type="text" aria-not-real="true" />
+<div role="region" aria-live="bogus">Inaccessible live region</div>',
+<button type="submit" aria-invalid={{if this.foo "true" "woosh"}}>Submit</button>
 ```
 
 This rule **allows** the following:
 
 ```hbs
 <input type="text" aria-required="true" />
+<div role="region" aria-live="polite">Accessible live region</div>',
+<button type="submit" aria-invalid={{if this.hasNoSpellingErrors "false" "spelling"}}>Send now</button>
 ```
 
 ## References
--- test/unit/rules/no-invalid-aria-attributes-test.js
+++ test/unit/rules/no-invalid-aria-attributes-test.js
@@ -10,9 +10,12 @@ generateRuleTests({
   good: [
     '<h1 aria-hidden="true">Valid Heading</h1>',
     '<input type="email" aria-required="true" />',
-    '<div role="slider" aria-valuenow={{this.foo}} aria-valuemax={{this.bar}} aria-valuemin={{this.baz}} />',
-    '<div role="region" aria-live="polite">Valid live region</div>',
+    '<div role="slider" aria-valuenow="50" aria-valuemax="100" aria-valuemin="0" />',
+    '<div role="region" aria-live="polite" aria-relevant="additions text">Valid live region</div>',
     '<CustomComponent @ariaRequired={{this.ariaRequired}} aria-errormessage="errorId" />',
+    '<div role="textbox" aria-sort={{if this.hasCustomSort "other" "ascending"}}></div>',
+    '<input type="text" aria-labelledby="label1 label2" />',
+    '<button type="submit" aria-disabled={{this.isDisabled}}>Submit</button>',
   ],
 
   bad: [
@@ -77,7 +80,6 @@ generateRuleTests({
         `);
       },
     },
-
     {
       template: '<CustomComponent role="region" aria-alert="polite" />',
       verifyResults(results) {
@@ -93,6 +95,128 @@ generateRuleTests({
               "rule": "no-invalid-aria-attributes",
               "severity": 2,
               "source": "<CustomComponent role=\\"region\\" aria-alert=\\"polite\\" />",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template:
+        '<span role="checkbox" aria-checked="bad-value" tabindex="0" aria-label="Forget me"></span>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 0,
+              "endColumn": 90,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The value for aria-checked must be a boolean or the string \\"mixed\\".",
+              "rule": "no-invalid-aria-attributes",
+              "severity": 2,
+              "source": "<span role=\\"checkbox\\" aria-checked=\\"bad-value\\" tabindex=\\"0\\" aria-label=\\"Forget me\\"></span>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<button type="submit" disabled="true" aria-disabled="123">Submit</button>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 0,
+              "endColumn": 73,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The value for aria-disabled must be a boolean.",
+              "rule": "no-invalid-aria-attributes",
+              "severity": 2,
+              "source": "<button type=\\"submit\\" disabled=\\"true\\" aria-disabled=\\"123\\">Submit</button>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<div role="region" aria-live="no-such-value">Inaccessible live region</div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 0,
+              "endColumn": 75,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The value for aria-live must be a single token from the following: assertive, off, polite.",
+              "rule": "no-invalid-aria-attributes",
+              "severity": 2,
+              "source": "<div role=\\"region\\" aria-live=\\"no-such-value\\">Inaccessible live region</div>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<div role="heading" aria-level="bogus">Inaccessible heading</div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 0,
+              "endColumn": 65,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The value for aria-level must be an integer.",
+              "rule": "no-invalid-aria-attributes",
+              "severity": 2,
+              "source": "<div role=\\"heading\\" aria-level=\\"bogus\\">Inaccessible heading</div>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<input type="text" aria-required={{if this.foo "true" "woosh"}} />',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 0,
+              "endColumn": 66,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The value for aria-required must be a boolean.",
+              "rule": "no-invalid-aria-attributes",
+              "severity": 2,
+              "source": "<input type=\\"text\\" aria-required={{if this.foo \\"true\\" \\"woosh\\"}} />",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template:
+        '<div role="region" aria-live="polite" aria-relevant="additions errors">Inaccessible live region</div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 0,
+              "endColumn": 101,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The value for aria-relevant must be a list of one or more tokens from the following: additions, all, removals, text.",
+              "rule": "no-invalid-aria-attributes",
+              "severity": 2,
+              "source": "<div role=\\"region\\" aria-live=\\"polite\\" aria-relevant=\\"additions errors\\">Inaccessible live region</div>",
             },
           ]
         `);
