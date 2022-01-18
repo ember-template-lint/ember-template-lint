6dea2064db600735abea7601ce0b59ac0241c513
Bert De Block <bert.deblock@bagaar.be>
2022-01-18 11:21:22 +0100

    Make sure the `modifier-name-case` rule supports the `modifier` helper

--- test/unit/rules/modifier-name-case-test.js
+++ test/unit/rules/modifier-name-case-test.js
@@ -14,6 +14,9 @@ generateRuleTests({
     '<button onclick={{doAThing "foo"}}></button>',
     '<a href="#" onclick={{amazingActionThing "foo"}} {{did-insert}}></a>',
     '<div didInsert></div>',
+    '<div {{(modifier "foo-bar")}}></div>',
+    '<div {{(if this.foo (modifier "foo-bar"))}}></div>',
+    '<div {{(modifier this.fooBar)}}></div>',
   ],
 
   bad: [
@@ -72,6 +75,46 @@ generateRuleTests({
               "rule": "modifier-name-case",
               "severity": 2,
               "source": "doSomething",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<div {{(modifier "fooBar")}}></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 17,
+              "endColumn": 25,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Use dasherized names for modifier invocation. Please replace \`fooBar\` with \`foo-bar\`.",
+              "rule": "modifier-name-case",
+              "severity": 2,
+              "source": "\\"fooBar\\"",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<div {{(if this.foo (modifier "fooBar"))}}></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "column": 30,
+              "endColumn": 38,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Use dasherized names for modifier invocation. Please replace \`fooBar\` with \`foo-bar\`.",
+              "rule": "modifier-name-case",
+              "severity": 2,
+              "source": "\\"fooBar\\"",
             },
           ]
         `);
