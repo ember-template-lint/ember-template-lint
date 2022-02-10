a1e774421b66c19a4276c07094eaa0c8ccbfd197
Jeff Sturgis <jesturgis@linkedin.com>
2022-02-10 10:02:25 -0800

    Use verifyResults instead of the old style results in tests.

--- docs/rule/no-obscure-array-access.md
+++ docs/rule/no-obscure-array-access.md
@@ -13,11 +13,11 @@ This rule **forbids** the following:
 ```
 
 ```hbs
-{{foo bar @list.[1]}}
+{{foo bar=@list.[1]}}
 ```
 
 ```hbs
-<Foo @bar={{list.[0]}} />
+<Foo @bar={{@list.[0]}} />
 ```
 
 This rule **allows** the following:
@@ -27,13 +27,13 @@ This rule **allows** the following:
 ```
 
 ```hbs
-{{foo bar (get @list '1')}}
+{{foo bar=(get @list '1')}}
 ```
 
 ```hbs
-<Foo @bar={{get list '0'}} />
+<Foo @bar={{get @list '0'}} />
 ```
 
 ## References
 
-- [Ember discord discussion in ember-cli channel on 02/06/19](https://discord.com/channels/480462759797063690/486548111221719040)
+- [Ember discord discussion in ember-cli channel on 02/06/19](https://discord.com/channels/480462759797063690/486548111221719040/542753450144956436)
--- test/unit/rules/no-obscure-array-access-test.js
+++ test/unit/rules/no-obscure-array-access-test.js
@@ -1,8 +1,9 @@
 import { ERROR_MESSAGE } from '../../../lib/rules/no-obscure-array-access.js';
 import generateRuleTests from '../../helpers/rule-test-harness.js';
 
+const RULE_NAME = 'no-obscure-array-access';
 generateRuleTests({
-  name: 'no-obscure-array-access',
+  name: RULE_NAME,
 
   config: true,
 
@@ -18,47 +19,102 @@ generateRuleTests({
   bad: [
     {
       template: '{{foo bar=this.list.[0]}}',
-      result: {
-        message: ERROR_MESSAGE('this.list.0'),
-        line: 1,
-        column: 10,
-        source: 'this.list.0',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 10,
+              "endColumn": 23,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${ERROR_MESSAGE('this.list.0')}",
+              "rule": "${RULE_NAME}",
+              "severity": 2,
+              "source": "this.list.0",
+            },
+          ]
+        `);
       },
     },
     {
       template: '{{foo bar @list.[1]}}',
-      result: {
-        message: ERROR_MESSAGE('@list.1'),
-        line: 1,
-        column: 10,
-        source: '@list.1',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 10,
+              "endColumn": 19,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${ERROR_MESSAGE('@list.1')}",
+              "rule": "${RULE_NAME}",
+              "severity": 2,
+              "source": "@list.1",
+            },
+          ]
+        `);
       },
     },
     {
       template: '{{this.list.[0]}}',
-      result: {
-        message: ERROR_MESSAGE('this.list.0'),
-        line: 1,
-        column: 2,
-        source: 'this.list.0',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 2,
+              "endColumn": 15,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${ERROR_MESSAGE('this.list.0')}",
+              "rule": "${RULE_NAME}",
+              "severity": 2,
+              "source": "this.list.0",
+            },
+          ]
+        `);
       },
     },
     {
       template: '{{this.list.[0].name}}',
-      result: {
-        message: ERROR_MESSAGE('this.list.0.name'),
-        line: 1,
-        column: 2,
-        source: 'this.list.0.name',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 2,
+              "endColumn": 20,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${ERROR_MESSAGE('this.list.0.name')}",
+              "rule": "${RULE_NAME}",
+              "severity": 2,
+              "source": "this.list.0.name",
+            },
+          ]
+        `);
       },
     },
     {
       template: '<Foo @bar={{this.list.[0]}} />',
-      result: {
-        message: ERROR_MESSAGE('this.list.0'),
-        line: 1,
-        column: 12,
-        source: 'this.list.0',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 12,
+              "endColumn": 25,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${ERROR_MESSAGE('this.list.0')}",
+              "rule": "${RULE_NAME}",
+              "severity": 2,
+              "source": "this.list.0",
+            },
+          ]
+        `);
       },
     },
   ],
