fd1896e731435c9aa7743facdb4a9ab9df645465
Judith Lung <judithhin@gmail.com>
2022-05-03 16:33:30 -0700

    Updated tests

--- test/unit/rules/require-media-caption-test.js
+++ test/unit/rules/require-media-caption-test.js
@@ -16,47 +16,122 @@ generateRuleTests({
   bad: [
     {
       template: '<video></video>',
-      result: {
-        message: 'Media elements such as <audio> and <video> must have a <track> for captions.',
-        line: 1,
-        column: 0,
-        source: '<video></video>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 15,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Media elements such as <audio> and <video> must have a <track> for captions.",
+              "rule": "require-media-caption",
+              "severity": 2,
+              "source": "<video></video>",
+            },
+          ]
+        `);
       },
     },
     {
       template: '<audio><track /></audio>',
-      result: {
-        message: 'Media elements such as <audio> and <video> must have a <track> for captions.',
-        line: 1,
-        column: 0,
-        source: '<audio><track /></audio>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 24,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Media elements such as <audio> and <video> must have a <track> for captions.",
+              "rule": "require-media-caption",
+              "severity": 2,
+              "source": "<audio><track /></audio>",
+            },
+          ]
+        `);
       },
     },
     {
       template: '<video><track kind="subtitles" /></video>',
-      result: {
-        message: 'Media elements such as <audio> and <video> must have a <track> for captions.',
-        line: 1,
-        column: 0,
-        source: '<video><track kind="subtitles" /></video>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 41,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Media elements such as <audio> and <video> must have a <track> for captions.",
+              "rule": "require-media-caption",
+              "severity": 2,
+              "source": "<video><track kind=\\"subtitles\\" /></video>",
+            },
+          ]
+        `);
       },
     },
     {
       template: '<audio muted="false"></audio>',
-      result: {
-        message: 'Media elements such as <audio> and <video> must have a <track> for captions.',
-        line: 1,
-        column: 0,
-        source: '<audio muted="false"></audio>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 29,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Media elements such as <audio> and <video> must have a <track> for captions.",
+              "rule": "require-media-caption",
+              "severity": 2,
+              "source": "<audio muted=\\"false\\"></audio>",
+            },
+          ]
+        `);
       },
     },
     {
       template: '<audio muted="false"><track kind="descriptions" /></audio>',
-      result: {
-        message: 'Media elements such as <audio> and <video> must have a <track> for captions.',
-        line: 1,
-        column: 0,
-        source: '<audio muted="false"><track kind="descriptions" /></audio>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 58,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Media elements such as <audio> and <video> must have a <track> for captions.",
+              "rule": "require-media-caption",
+              "severity": 2,
+              "source": "<audio muted=\\"false\\"><track kind=\\"descriptions\\" /></audio>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<video muted=false></video>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 27,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Media elements such as <audio> and <video> must have a <track> for captions.",
+              "rule": "require-media-caption",
+              "severity": 2,
+              "source": "<video muted=false></video>",
+            },
+          ]
+        `);
       },
     },
   ],
