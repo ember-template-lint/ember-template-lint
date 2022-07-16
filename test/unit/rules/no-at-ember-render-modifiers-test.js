d01b76575d9a41548652cf04ca8b70c19f682a77
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-16 12:08:07 -0400

    Update tests

--- test/unit/rules/no-at-ember-render-modifiers-test.js
+++ test/unit/rules/no-at-ember-render-modifiers-test.js
@@ -11,20 +11,59 @@ generateRuleTests({
   bad: [
     {
       template: '<div {{did-insert}}></div>',
-
       verifyResults(results) {
         expect(results).toMatchInlineSnapshot(`
           [
             {
-              "column": 8,
-              "endColumn": 24,
+              "column": 5,
+              "endColumn": 19,
               "endLine": 1,
               "filePath": "layout.hbs",
               "line": 1,
-              "message": ${MESSAGES['did-insert']},
-              "rule": "no-action-modifiers",
+              "message": "${MESSAGES['did-insert']}",
+              "rule": "no-at-ember-render-modifiers",
               "severity": 2,
-              "source": "<button {{action \\"foo\\"}}></button>",
+              "source": "<div {{did-insert}}></div>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<div {{did-update}}></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 5,
+              "endColumn": 19,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${MESSAGES['did-update']}",
+              "rule": "no-at-ember-render-modifiers",
+              "severity": 2,
+              "source": "<div {{did-update}}></div>",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: '<div {{will-destroy}}></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 5,
+              "endColumn": 21,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "${MESSAGES['will-destroy']}",
+              "rule": "no-at-ember-render-modifiers",
+              "severity": 2,
+              "source": "<div {{will-destroy}}></div>",
             },
           ]
         `);
