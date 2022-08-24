8aa0313b2eea4c580b2eadd330a3235fd3c1121e
Ricardo Mendes <rokusu@gmail.com>
2022-08-24 14:32:16 +0100

    draft

--- test/unit/rules/linebreak-style-test.js
+++ test/unit/rules/linebreak-style-test.js
@@ -34,179 +34,179 @@ generateRuleTests({
   ],
 
   bad: [
-    // {
-    //   template: 'something\ngoes\r\n',
-    //   fixedTemplate: 'something\ngoes\n',
+    {
+      template: 'something\ngoes\r\n',
+      fixedTemplate: 'something\ngoes\n',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 4,
-    //           "endColumn": 0,
-    //           "endLine": 3,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": true,
-    //           "line": 2,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
-    // {
-    //   config: 'unix',
-    //   template: '\r\n',
-    //   fixedTemplate: '\n',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 4,
+              "endColumn": 0,
+              "endLine": 3,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 2,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
+      template: '\r\n',
+      fixedTemplate: '\n',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 0,
-    //           "endColumn": 0,
-    //           "endLine": 2,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": true,
-    //           "line": 1,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
-    // {
-    //   config: 'unix',
-    //   template: '{{#if test}}\r\n{{/if}}',
-    //   fixedTemplate: '{{#if test}}\n{{/if}}',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 0,
+              "endColumn": 0,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
+      template: '{{#if test}}\r\n{{/if}}',
+      fixedTemplate: '{{#if test}}\n{{/if}}',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 12,
-    //           "endColumn": 0,
-    //           "endLine": 2,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": true,
-    //           "line": 1,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
-    // {
-    //   config: 'unix',
-    //   template: '{{blah}}\r\n{{blah}}',
-    //   fixedTemplate: '{{blah}}\n{{blah}}',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 12,
+              "endColumn": 0,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
+      template: '{{blah}}\r\n{{blah}}',
+      fixedTemplate: '{{blah}}\n{{blah}}',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 8,
-    //           "endColumn": 0,
-    //           "endLine": 2,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": true,
-    //           "line": 1,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
-    // {
-    //   config: 'unix',
-    //   template: '{{blah}}\r\n',
-    //   fixedTemplate: '{{blah}}\n',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 8,
+              "endColumn": 0,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
+      template: '{{blah}}\r\n',
+      fixedTemplate: '{{blah}}\n',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 8,
-    //           "endColumn": 0,
-    //           "endLine": 2,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": true,
-    //           "line": 1,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
-    // {
-    //   config: 'unix',
-    //   template: '{{blah arg="\r\n"}}',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 8,
+              "endColumn": 0,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
+      template: '{{blah arg="\r\n"}}',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 12,
-    //           "endColumn": 3,
-    //           "endLine": 2,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": false,
-    //           "line": 1,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
-    // {
-    //   config: 'unix',
-    //   template: '<blah arg="\r\n" />',
-    //   fixedTemplate: '<blah arg="\n" />',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 12,
+              "endColumn": 3,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": false,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
+      template: '<blah arg="\r\n" />',
+      fixedTemplate: '<blah arg="\n" />',
 
-    //   verifyResults(results) {
-    //     expect(results).toMatchInlineSnapshot(`
-    //       [
-    //         {
-    //           "column": 11,
-    //           "endColumn": 0,
-    //           "endLine": 2,
-    //           "filePath": "layout.hbs",
-    //           "isFixable": true,
-    //           "line": 1,
-    //           "message": "Wrong linebreak used. Expected LF but found CRLF",
-    //           "rule": "linebreak-style",
-    //           "severity": 2,
-    //           "source": "
-    //       ",
-    //         },
-    //       ]
-    //     `);
-    //   },
-    // },
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 11,
+              "endColumn": 0,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
     {
       config: 'windows',
       template: '1\n2',
@@ -259,6 +259,31 @@ generateRuleTests({
     },
     {
       config: 'unix',
+      template: '<!-- comment\r\nline -->',
+      fixedTemplate: '<!-- comment\nline -->',
+
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 12,
+              "endColumn": 8,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'unix',
       template: '{{!-- comment\r\nline --}}',
       fixedTemplate: '{{!-- comment\nline --}}',
 
@@ -273,6 +298,31 @@ generateRuleTests({
               "isFixable": true,
               "line": 1,
               "message": "Wrong linebreak used. Expected LF but found CRLF",
+              "rule": "linebreak-style",
+              "severity": 2,
+              "source": "
+          ",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      config: 'windows',
+      template: '{{!-- comment\nline --}}',
+      fixedTemplate: '{{!-- comment\r\nline --}}',
+
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 13,
+              "endColumn": 9,
+              "endLine": 2,
+              "filePath": "layout.hbs",
+              "isFixable": true,
+              "line": 1,
+              "message": "Wrong linebreak used. Expected CRLF but found LF",
               "rule": "linebreak-style",
               "severity": 2,
               "source": "
