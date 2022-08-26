f2f65d2bc6ef247767ab37848c68d67b7c958ac3
Ernesto Mudu <emudu@emudu-mn1.linkedin.biz>
2022-08-26 17:14:20 -0400

    Complete isStrictMode implementation and add tests.

--- test/unit/extract-templates-test.js
+++ test/unit/extract-templates-test.js
@@ -1,9 +1,23 @@
-import { coordinatesOf, extractTemplates } from '../../lib/extract-templates.js';
+import * as util from 'ember-template-imports/src/util.js';
+
+import { coordinatesOf, extractTemplates, isStrictMode } from '../../lib/extract-templates.js';
 
 describe('extractTemplates', function () {
   const handlebarsTemplate = '<div></div>';
   const script =
     'export const SomeComponent = <template>\n' + '<button></button>\n' + '</template>';
+  const scriptTemplateLiteral = `import { hbs } from 'ember-cli-htmlbars';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`;
+  const scriptTemplateLiteralStrictMode = `import { hbs } from 'ember-template-imports';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`;
+  const scriptTemplateLiteralStrictModeAliased = `import { hbs as theHbs } from 'ember-template-imports';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(theHbs\`{{book}}\`, templateOnly());`;
 
   describe('extractTemplates with relativePath undefined (receiving input from stdin)', function () {
     it('returns the entire content if the content could be parsed as a script', function () {
@@ -12,26 +26,49 @@ describe('extractTemplates', function () {
           column: 0,
           line: 0,
           isEmbedded: undefined,
+          isStrictMode: true,
           template: handlebarsTemplate,
         },
       ]);
     });
-    it.only('returns the parsed template', function () {
-      const embedded = `import { hbs } from 'ember-cli-htmlbars';
-      import { setComponentTemplate } from '@ember/component';
-      import templateOnly from '@ember/component/template-only';
-
-      export const SomeComponent = setComponentTemplate(hbs\`
-          <button></button>
-          \`,
-          templateOnly()
-      );`;
-      expect(extractTemplates(embedded)).toEqual([
+    it('isStrictMode is false for template literal that do not require strict mode', function () {
+      expect(extractTemplates(scriptTemplateLiteral)).toEqual([
         {
-          column: 39,
-          line: 1,
+          column: 60,
+          line: 4,
           isEmbedded: true,
-          template: '\n<button></button>\n',
+          isStrictMode: false,
+          template: '{{book}}',
+        },
+      ]);
+    });
+    it('isStrictMode is true for template literal that requires strict mode', function () {
+      expect(extractTemplates(scriptTemplateLiteralStrictMode)).toEqual([
+        {
+          column: 60,
+          line: 4,
+          isEmbedded: true,
+          isStrictMode: true,
+          template: '{{book}}',
+        },
+      ]);
+    });
+    /* TODO
+     * when the import identifier is aliased as in:
+     * import { hbs as theHbs } from 'ember-template-imports';
+     * parseTemplates returns:
+     * { importIdentifier: theHbs, importPath: 'ember-template-imports'
+     * as isStrictMode() checks for importIdentifier === 'hbs' this case is interpreted as isStrictMode = false.
+     *
+     */
+    it('isStrictMode is true for template literal that requires strict mode when the template identifier is aliased', function () {
+      expect(extractTemplates(scriptTemplateLiteralStrictModeAliased)).toEqual([
+        {
+          column: 63,
+          line: 4,
+          isEmbedded: true,
+          isStrictMode: true,
+          template: '{{book}}',
         },
       ]);
     });
@@ -41,6 +78,7 @@ describe('extractTemplates', function () {
           column: 39,
           line: 1,
           isEmbedded: true,
+          isStrictMode: true,
           template: '\n<button></button>\n',
         },
       ]);
@@ -54,6 +92,7 @@ describe('extractTemplates', function () {
           column: 0,
           line: 0,
           isEmbedded: undefined,
+          isStrictMode: true,
           template: handlebarsTemplate,
         },
       ]);
@@ -64,6 +103,7 @@ describe('extractTemplates', function () {
           column: 39,
           line: 1,
           isEmbedded: true,
+          isStrictMode: true,
           template: '\n<button></button>\n',
         },
       ]);
@@ -89,5 +129,28 @@ describe('calculate template coordinates', function () {
       line: 8,
       column: 12,
     });
+  });
+});
+
+describe('isStrictMode', function () {
+  it.each([
+    {
+      importIdentifier: util.TEMPLATE_LITERAL_IDENTIFIER,
+      importPath: util.TEMPLATE_LITERAL_MODULE_SPECIFIER,
+    },
+    { type: 'template-tag' },
+  ])('isStrictMode returns true', function (templateInfo) {
+    expect(isStrictMode(templateInfo)).toBe(true);
+  });
+  it.each([
+    { importIdentifier: 'hbs', importPath: 'ember-cli-htmlbars' },
+    { importIdentifier: 'hbs', importPath: '@ember/template-compilation' },
+    { importIdentifier: 'hbs', importPath: '@ember/template-compilation' },
+    { importIdentifier: 'default', importPath: 'ember-cli-htmlbars-inline-precompile' },
+    { importIdentifier: 'default', importPath: 'htmlbars-inline-precompile' },
+    { importIdentifier: 'precompileTemplate', importPath: '@ember/template-compilation' },
+    { type: 'template-literal' },
+  ])('isStrictMode returns false', function (templateInfo) {
+    expect(isStrictMode(templateInfo)).toBe(false);
   });
 });
--- test/unit/rules/no-implicit-this-test.js
+++ test/unit/rules/no-implicit-this-test.js
@@ -29,25 +29,38 @@ let good = [
     template: '{{foo-bar data-test-foo}}',
   },
   {
-    template: '{{book}}',
+    template: `import { hbs } from 'ember-cli-htmlbars';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`,
     meta: {
       filePath: 'layout.gjs',
     },
   },
   {
-    template: '{{book}}',
+    template: `import { hbs } from 'ember-cli-htmlbars';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`,
     meta: {
       filePath: 'layout.gts',
     },
   },
   {
-    template: '{{book}}',
+    config: { isStrictMode: false },
+    template: `import { hbs } from 'ember-cli-htmlbars';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`,
     meta: {
       filePath: 'layout.js',
     },
   },
   {
-    template: '{{book}}',
+    template: `import { hbs } from 'ember-cli-htmlbars';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`,
     meta: {
       filePath: 'layout.ts',
     },
@@ -255,19 +268,71 @@ generateRuleTests({
       },
     },
     {
-      template: '{{book}}',
+      template: '<template>{{book}}</template>',
       meta: {
-        filePath: undefined,
+        filePath: 'layout.gjs',
       },
       verifyResults(results) {
         expect(results).toMatchInlineSnapshot(`
           [
             {
-              "column": 2,
-              "endColumn": 6,
+              "column": 12,
+              "endColumn": 16,
               "endLine": 1,
-              "filePath": "layout.hbs",
+              "filePath": "layout.gjs",
               "line": 1,
+              "message": "Ambiguous path 'book' is not allowed. Use '@book' if it is a named argument or 'this.book' if it is a property on 'this'. If it is a helper or component that has no arguments, you must either convert it to an angle bracket invocation or manually add it to the 'no-implicit-this' rule configuration, e.g. 'no-implicit-this': { allow: ['book'] }.",
+              "rule": "no-implicit-this",
+              "severity": 2,
+              "source": "book",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      meta: {
+        filePath: 'layout.gjs',
+      },
+      template: `import { hbs } from 'ember-template-imports';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(hbs\`{{book}}\`, templateOnly());`,
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 62,
+              "endColumn": 66,
+              "endLine": 4,
+              "filePath": "layout.gjs",
+              "line": 4,
+              "message": "Ambiguous path 'book' is not allowed. Use '@book' if it is a named argument or 'this.book' if it is a property on 'this'. If it is a helper or component that has no arguments, you must either convert it to an angle bracket invocation or manually add it to the 'no-implicit-this' rule configuration, e.g. 'no-implicit-this': { allow: ['book'] }.",
+              "rule": "no-implicit-this",
+              "severity": 2,
+              "source": "book",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      meta: {
+        filePath: 'layout.gjs',
+      },
+      template: `import { hbs as theHbs } from 'ember-template-imports';
+      import { setComponentTemplate } from '@ember/component';
+      import templateOnly from '@ember/component/template-only';
+      export const SomeComponent = setComponentTemplate(theHbs\`{{book}}\`, templateOnly());`,
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 62,
+              "endColumn": 66,
+              "endLine": 4,
+              "filePath": "layout.gjs",
+              "line": 4,
               "message": "Ambiguous path 'book' is not allowed. Use '@book' if it is a named argument or 'this.book' if it is a property on 'this'. If it is a helper or component that has no arguments, you must either convert it to an angle bracket invocation or manually add it to the 'no-implicit-this' rule configuration, e.g. 'no-implicit-this': { allow: ['book'] }.",
               "rule": "no-implicit-this",
               "severity": 2,
