25506811a5d8b255423c1a90b839568245b6c6c0
Gabriel J. Csapo <gabecsapo@gmail.com>
2022-11-09 11:31:50 -0800

    WIP

--- test/unit/rules/require-button-type-test.js
+++ test/unit/rules/require-button-type-test.js
@@ -6,165 +6,154 @@ generateRuleTests({
   config: true,
 
   good: [
-    // valid static values
-    '<button type="button" />',
-    '<button type="button">label</button>',
-    '<button type="submit" />',
-    '<button type="reset" />',
-    // dynamic values
-    '<button type="{{buttonType}}" />',
-    '<button type={{buttonType}} />',
-    '<div/>',
-    '<div></div>',
-    '<div type="randomType"></div>',
+    // // valid static values
+    // '<button type="button" />',
+    // '<button type="button">label</button>',
+    // '<button type="submit" />',
+    // '<button type="reset" />',
+    // // dynamic values
+    // '<button type="{{buttonType}}" />',
+    // '<button type={{buttonType}} />',
+    // '<div/>',
+    // '<div></div>',
+    // '<div type="randomType"></div>',
   ],
 
   bad: [
-    {
-      template: '<button/>',
-      fixedTemplate: '<button type="button" />',
+    // {
+    //   template: '<button/>',
+    //   fixedTemplate: '<button type="button" />',
 
-      verifyResults(results) {
-        expect(results).toMatchInlineSnapshot(`
-          [
-            {
-              "column": 0,
-              "endColumn": 9,
-              "endLine": 1,
-              "filePath": "layout.hbs",
-              "isFixable": true,
-              "line": 1,
-              "message": "All \`<button>\` elements should have a valid \`type\` attribute",
-              "rule": "require-button-type",
-              "severity": 2,
-              "source": "<button/>",
-            },
-          ]
-        `);
-      },
-    },
-    {
-      template: '<button>label</button>',
-      fixedTemplate: '<button type="button">label</button>',
+    //   verifyResults(results) {
+    //     expect(results).toMatchInlineSnapshot(`
+    //       [
+    //         {
+    //           "column": 0,
+    //           "endColumn": 9,
+    //           "endLine": 1,
+    //           "filePath": "layout.hbs",
+    //           "isFixable": true,
+    //           "line": 1,
+    //           "message": "All \`<button>\` elements should have a valid \`type\` attribute",
+    //           "rule": "require-button-type",
+    //           "severity": 2,
+    //           "source": "<button/>",
+    //         },
+    //       ]
+    //     `);
+    //   },
+    // },
+    // {
+    //   template: '<button>label</button>',
+    //   fixedTemplate: '<button type="button">label</button>',
 
-      verifyResults(results) {
-        expect(results).toMatchInlineSnapshot(`
-          [
-            {
-              "column": 0,
-              "endColumn": 22,
-              "endLine": 1,
-              "filePath": "layout.hbs",
-              "isFixable": true,
-              "line": 1,
-              "message": "All \`<button>\` elements should have a valid \`type\` attribute",
-              "rule": "require-button-type",
-              "severity": 2,
-              "source": "<button>label</button>",
-            },
-          ]
-        `);
-      },
-    },
-    {
-      template: '<button type="" />',
-      fixedTemplate: '<button type="button" />',
+    //   verifyResults(results) {
+    //     expect(results).toMatchInlineSnapshot(`
+    //       [
+    //         {
+    //           "column": 0,
+    //           "endColumn": 22,
+    //           "endLine": 1,
+    //           "filePath": "layout.hbs",
+    //           "isFixable": true,
+    //           "line": 1,
+    //           "message": "All \`<button>\` elements should have a valid \`type\` attribute",
+    //           "rule": "require-button-type",
+    //           "severity": 2,
+    //           "source": "<button>label</button>",
+    //         },
+    //       ]
+    //     `);
+    //   },
+    // },
+    // {
+    //   template: '<button type="" />',
+    //   fixedTemplate: '<button type="button" />',
 
-      verifyResults(results) {
-        expect(results).toMatchInlineSnapshot(`
-          [
-            {
-              "column": 0,
-              "endColumn": 18,
-              "endLine": 1,
-              "filePath": "layout.hbs",
-              "isFixable": true,
-              "line": 1,
-              "message": "All \`<button>\` elements should have a valid \`type\` attribute",
-              "rule": "require-button-type",
-              "severity": 2,
-              "source": "<button type=\\"\\" />",
-            },
-          ]
-        `);
-      },
-    },
-    {
-      template: '<button type="foo" />',
-      fixedTemplate: '<button type="button" />',
+    //   verifyResults(results) {
+    //     expect(results).toMatchInlineSnapshot(`
+    //       [
+    //         {
+    //           "column": 0,
+    //           "endColumn": 18,
+    //           "endLine": 1,
+    //           "filePath": "layout.hbs",
+    //           "isFixable": true,
+    //           "line": 1,
+    //           "message": "All \`<button>\` elements should have a valid \`type\` attribute",
+    //           "rule": "require-button-type",
+    //           "severity": 2,
+    //           "source": "<button type=\\"\\" />",
+    //         },
+    //       ]
+    //     `);
+    //   },
+    // },
+    // {
+    //   template: '<button type="foo" />',
+    //   fixedTemplate: '<button type="button" />',
 
-      verifyResults(results) {
-        expect(results).toMatchInlineSnapshot(`
-          [
-            {
-              "column": 0,
-              "endColumn": 21,
-              "endLine": 1,
-              "filePath": "layout.hbs",
-              "isFixable": true,
-              "line": 1,
-              "message": "All \`<button>\` elements should have a valid \`type\` attribute",
-              "rule": "require-button-type",
-              "severity": 2,
-              "source": "<button type=\\"foo\\" />",
-            },
-          ]
-        `);
-      },
-    },
-    {
-      template: '<button type=42 />',
-      fixedTemplate: '<button type="button" />',
+    //   verifyResults(results) {
+    //     expect(results).toMatchInlineSnapshot(`
+    //       [
+    //         {
+    //           "column": 0,
+    //           "endColumn": 21,
+    //           "endLine": 1,
+    //           "filePath": "layout.hbs",
+    //           "isFixable": true,
+    //           "line": 1,
+    //           "message": "All \`<button>\` elements should have a valid \`type\` attribute",
+    //           "rule": "require-button-type",
+    //           "severity": 2,
+    //           "source": "<button type=\\"foo\\" />",
+    //         },
+    //       ]
+    //     `);
+    //   },
+    // },
+    // {
+    //   template: '<button type=42 />',
+    //   fixedTemplate: '<button type="button" />',
 
-      verifyResults(results) {
-        expect(results).toMatchInlineSnapshot(`
-          [
-            {
-              "column": 0,
-              "endColumn": 18,
-              "endLine": 1,
-              "filePath": "layout.hbs",
-              "isFixable": true,
-              "line": 1,
-              "message": "All \`<button>\` elements should have a valid \`type\` attribute",
-              "rule": "require-button-type",
-              "severity": 2,
-              "source": "<button type=42 />",
-            },
-          ]
-        `);
-      },
-    },
+    //   verifyResults(results) {
+    //     expect(results).toMatchInlineSnapshot(`
+    //       [
+    //         {
+    //           "column": 0,
+    //           "endColumn": 18,
+    //           "endLine": 1,
+    //           "filePath": "layout.hbs",
+    //           "isFixable": true,
+    //           "line": 1,
+    //           "message": "All \`<button>\` elements should have a valid \`type\` attribute",
+    //           "rule": "require-button-type",
+    //           "severity": 2,
+    //           "source": "<button type=42 />",
+    //         },
+    //       ]
+    //     `);
+    //   },
+    // },
+    // {
+    //   template: '<form><button></button></form>',
+    //   fixedTemplate: '<form><button type="submit"></button></form>',
+    // },
     {
-      template: '<form><button></button></form>',
-      fixedTemplate: '<form><button type="submit"></button></form>',
-    },
-    {
-      template: '/** silly example <button> usage */ <template><button><button></template>',
+      template: '/** silly example </button> usage */ <template><button></button></template>',
       meta: {
         filePath: 'layout.gjs',
       },
       fixedTemplate:
         '/** silly example <button> usage */ <template><button type="button"></button></template>',
+      skipDisabledTests: true,
 
       verifyResults(results) {
         expect(results).toMatchInlineSnapshot(`
           [
             {
-              "column": 28,
-              "endColumn": 37,
-              "endLine": 1,
-              "filePath": "layout.gjs",
-              "isFixable": true,
-              "line": 1,
-              "message": "All \`<button>\` elements should have a valid \`type\` attribute",
-              "rule": "require-button-type",
-              "severity": 2,
-              "source": "<button/>",
-            },
-            {
-              "column": 47,
-              "endColumn": 64,
+              "column": 46,
+              "endColumn": 63,
               "endLine": 1,
               "filePath": "layout.gjs",
               "isFixable": true,
