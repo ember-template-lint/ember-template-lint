9b82d9e41e92e137beb55c4530ac090d564196c4
Raul Rivero <raul9501@gmail.com>
2023-05-17 21:00:30 -0400

    Add new rule simple-modifiers

---
+++ test/unit/rules/simple-modifiers-test.js
@@ -0,0 +1,38 @@
+import generateRuleTests from '../../helpers/rule-test-harness.js';
+
+generateRuleTests({
+  name: 'simple-modifiers',
+
+  config: true,
+
+  good: [
+    '<div {{(modifier "track-interaction" @controlName)}}></div>',
+    '<div {{(modifier this.trackInteraction @controlName)}}></div>',
+    '<div {{(modifier @trackInteraction @controlName)}}></div>',
+    '<div {{(if @isActionVisible (modifier "track-interaction" eventName=myEventName eventBody=myEventbody))}}></div>',
+  ],
+
+  bad: [
+    {
+      template:
+        '<div {{(modifier (unless this.hasBeenClicked "track-interaction") "click" customizeData=this.customizeClickData)}}></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 17,
+              "endColumn": 65,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "The modifier helper should have a string or a variable name containing the modifier name as a first argument.",
+              "rule": "simple-modifiers",
+              "severity": 2,
+              "source": "(unless this.hasBeenClicked \\"track-interaction\\")",
+            },
+          ]
+        `);
+      },
+    },
+  ],
+});
