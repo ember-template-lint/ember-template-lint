11ea4820a5f8aa7ff13f678388990796d8579fba
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-14 16:24:26 -0400

    docs

---
+++ docs/rule/no-jsx-attributes.md
@@ -0,0 +1,67 @@
+# no-jsx-attributes
+
+Folks coming from React may have developed habits around how they type attributes on elements.
+JSX isn't HTML (it's JS), so in JS, you can't have kebab-case identifiers, so JSX uses camelCase.
+
+However, since Ember uses HTML, camelCase attributes are not valid when writing components.
+
+## Examples
+
+This rule **forbids** the following attributes:
+
+```
+acceptCharset
+accessKey
+allowFullScreen
+allowTransparency
+autoComplete
+autoFocus
+autoPlay
+cellPadding
+cellSpacing
+charSet
+className
+contentEditable
+contextMenu
+crossOrigin
+dataTime
+encType
+formAction
+formEncType
+formMethod
+formNoValidate
+formTarget
+frameBorder
+httpEquiv
+inputMode
+keyParams
+keyType
+noValidate
+marginHeight
+marginWidth
+maxLength
+mediaGroup
+minLength
+radioGroup
+readOnly
+rowSpan
+spellCheck
+srcDoc
+srcSet
+tabIndex
+useMap
+```
+
+## Migration
+
+Convert attributes to kebab-case[^camelCaseNote]
+
+- `<div className="...">` -> `<div class="...">`
+- `<video autoPlay>` -> `<video auto-play>`
+- `<div contentEditable>` -> `<div content-editable>`
+- etc
+
+
+[^camelCaseNote]: keep in mind that `@args`, and `<:blocks>` should be js-compatible identifiers and be camelCase
+
+
--- test/unit/rules/no-jsx-attributes-test.js
+++ test/unit/rules/no-jsx-attributes-test.js
@@ -1,22 +1,98 @@
 import generateRuleTests from '../../helpers/rule-test-harness.js';
 
 generateRuleTests({
-  name: 'no-link-to-positional-params',
+  name: 'no-jsx-attributes',
 
   config: true,
 
-  good: ['<div></div>', '<div class="foo"></div>', '<div class></div>'],
+  good: [
+    '<div></div>',
+    '<div class="foo"></div>',
+    '<div class></div>',
+    '<div auto-play></div>',
+    '<div content-editable="true"></div>',
+  ],
   bad: [
     {
       template: '<div className></div>',
       verifyResults(results) {
-        expect(results).toMatchInlineSnapshot();
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 5,
+              "endColumn": 14,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Attribute, className, does not assign the 'class' attribute as it would in JSX.  To assign the 'class' attribute, set the 'class' attribute, instead of 'className'. In HTML, all attributes are valid, but 'className' doesn't do anything.",
+              "rule": "no-jsx-attributes",
+              "severity": 2,
+              "source": "className",
+            },
+          ]
+        `);
       },
     },
     {
       template: '<div className="foo"></div>',
       verifyResults(results) {
-        expect(results).toMatchInlineSnapshot();
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 5,
+              "endColumn": 20,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Attribute, className, does not assign the 'class' attribute as it would in JSX.  To assign the 'class' attribute, set the 'class' attribute, instead of 'className'. In HTML, all attributes are valid, but 'className' doesn't do anything.",
+              "rule": "no-jsx-attributes",
+              "severity": 2,
+              "source": "className=\\"foo\\"",
+            },
+          ]
+        `);
+      },
+    },
+
+    {
+      template: '<div autoPlay></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 5,
+              "endColumn": 13,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Attribute, autoPlay, is probably unintended. In HTML, all attributes are valid, but 'className' doesn't do anything.",
+              "rule": "no-jsx-attributes",
+              "severity": 2,
+              "source": "autoPlay",
+            },
+          ]
+        `);
+      },
+    },
+
+    {
+      template: '<div contentEditable></div>',
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 5,
+              "endColumn": 20,
+              "endLine": 1,
+              "filePath": "layout.hbs",
+              "line": 1,
+              "message": "Attribute, contentEditable, is probably unintended. In HTML, all attributes are valid, but 'className' doesn't do anything.",
+              "rule": "no-jsx-attributes",
+              "severity": 2,
+              "source": "contentEditable",
+            },
+          ]
+        `);
       },
     },
   ],
