75540c50b8e374eda134dcaee738f50c0534ba71
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-13 15:23:22 -0500

    Setup vitest wiht jest settings, remove jest

--- test/jest-setup.js
+++ test/jest-setup.js
@@ -1,2 +1,3 @@
+// https://github.com/microsoft/sarif-js-sdk/issues/38
 // eslint-disable-next-line import/no-unassigned-import
 import '@microsoft/jest-sarif';
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,17 @@ export default defineConfig({
   test: {
     // migration from jest
     globals: true,
+    include: ['test/**/*-test.js'],
+    setupFiles: ['test/jest-setup.js'],
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      thresholds: {
+        lines: 86,
+        functions: 91,
+        branches: 82,
+        statements: 86,
+      },
+    }
   },
 })
