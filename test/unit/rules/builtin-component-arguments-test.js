cd0e478fb3c68dffd76bb669c41520aa9032222b
Peter Wagenet <peter@wagenet.us>
2025-03-31 17:35:22 -0700

    Update builtin-component-arguments

--- test/unit/rules/builtin-component-arguments-test.js
+++ test/unit/rules/builtin-component-arguments-test.js
@@ -11,6 +11,23 @@ generateRuleTests({
     '<Input @type="text" size="10" />',
     '<Input @type="checkbox" @checked={{true}} />',
     '<Textarea @value="Tomster" />',
+    // Components from non-Ember locations should not be flagged
+    {
+      template: 'import { Input } from "my-custom-components";\n\n<template><Input type="text" /></template>',
+      meta: { filePath: 'template.gjs' },
+    },
+    {
+      template: 'import { Input } from "my-custom-components";\n\n<template><Input type="text" /></template>',
+      meta: { filePath: 'template.gts' },
+    },
+    {
+      template: 'import { Textarea } from "my-custom-components";\n\n<template><Textarea value="world" /></template>',
+      meta: { filePath: 'template.gjs' },
+    },
+    {
+      template: 'import { Textarea } from "my-custom-components";\n\n<template><Textarea value="world" /></template>',
+      meta: { filePath: 'template.gts' },
+    },
   ],
 
   bad: [
@@ -69,6 +86,91 @@ generateRuleTests({
               "rule": "builtin-component-arguments",
               "severity": 2,
               "source": "value="Tomster"",
+            },
+          ]
+        `);
+      },
+    },
+    // Components with aliases should be flagged with the original name
+    {
+      template: 'import { Input as CustomInput } from "@ember/component";\n\n<template><CustomInput type="text" /></template>',
+      meta: { filePath: 'template.gjs' },
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 23,
+              "endColumn": 34,
+              "endLine": 3,
+              "filePath": "template.gjs",
+              "line": 3,
+              "message": "Setting the \`type\` attribute on the builtin <Input> component is not allowed. Did you mean \`@type\`?",
+              "rule": "builtin-component-arguments",
+              "severity": 2,
+              "source": "type="text"",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: 'import { Input as CustomInput } from "@ember/component";\n\n<template><CustomInput type="text" /></template>',
+      meta: { filePath: 'template.gts' },
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 23,
+              "endColumn": 34,
+              "endLine": 3,
+              "filePath": "template.gts",
+              "line": 3,
+              "message": "Setting the \`type\` attribute on the builtin <Input> component is not allowed. Did you mean \`@type\`?",
+              "rule": "builtin-component-arguments",
+              "severity": 2,
+              "source": "type="text"",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: 'import { Textarea as CustomTextarea } from "@ember/component";\n\n<template><CustomTextarea value="world" /></template>',
+      meta: { filePath: 'template.gjs' },
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 26,
+              "endColumn": 39,
+              "endLine": 3,
+              "filePath": "template.gjs",
+              "line": 3,
+              "message": "Setting the \`value\` attribute on the builtin <Textarea> component is not allowed. Did you mean \`@value\`?",
+              "rule": "builtin-component-arguments",
+              "severity": 2,
+              "source": "value="world"",
+            },
+          ]
+        `);
+      },
+    },
+    {
+      template: 'import { Textarea as CustomTextarea } from "@ember/component";\n\n<template><CustomTextarea value="world" /></template>',
+      meta: { filePath: 'template.gts' },
+      verifyResults(results) {
+        expect(results).toMatchInlineSnapshot(`
+          [
+            {
+              "column": 26,
+              "endColumn": 39,
+              "endLine": 3,
+              "filePath": "template.gts",
+              "line": 3,
+              "message": "Setting the \`value\` attribute on the builtin <Textarea> component is not allowed. Did you mean \`@value\`?",
+              "rule": "builtin-component-arguments",
+              "severity": 2,
+              "source": "value="world"",
             },
           ]
         `);
