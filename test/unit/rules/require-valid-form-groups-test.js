3a8abbb42c497333b96c72b45e91b54a80e0f79f
Alex Kanunnikov <lifeart92@gmail.com>
2025-05-06 12:55:46 +0300

    chore: improve logic

--- test/unit/rules/require-valid-form-groups-test.js
+++ test/unit/rules/require-valid-form-groups-test.js
@@ -10,32 +10,49 @@ generateRuleTests({
       <legend>Preferred Mascot Version</legend>
       <div>
         <label for="radio-001">Chicago Zoey</label>
-        <input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey" />
+        <input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey">
       </div>
     </fieldset>`,
     `<div role="group" aria-labelledby="preferred-mascot-heading">
       <div id="preferred-mascot-heading">Preferred Mascot Version</div>
       <label for="radio-001">Chicago Zoey</label>
-      <input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey" />
+      <input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey">
+      <label for="radio-002">Chicago Tom</label>
+      <input id="radio-002" type="radio" name="prefMascot-Tom" value="chicago zoey">
+    </div>`,
+    `<div>
+      <label for="radio-001">Chicago Zoey</label>
+      <input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey">
     </div>`,
   ],
 
   bad: [
     {
-      template: '<div><label for="radio-001">Chicago Zoey</label></div>',
+      template: '<div><input name="a1">Chicago Zoey<input name="a2">Chicago Tom</div>',
       verifyResults(results) {
         expect(results).toMatchInlineSnapshot(`
  [
    {
      "column": 5,
-     "endColumn": 48,
+     "endColumn": 22,
      "endLine": 1,
      "filePath": "layout.hbs",
      "line": 1,
      "message": "Grouped form controls should have appropriate semantics such as fieldset and legend or WAI-ARIA labels",
      "rule": "require-valid-form-groups",
      "severity": 2,
-     "source": "<label for="radio-001">Chicago Zoey</label>",
+     "source": "<input name="a1">",
+   },
+   {
+     "column": 34,
+     "endColumn": 51,
+     "endLine": 1,
+     "filePath": "layout.hbs",
+     "line": 1,
+     "message": "Grouped form controls should have appropriate semantics such as fieldset and legend or WAI-ARIA labels",
+     "rule": "require-valid-form-groups",
+     "severity": 2,
+     "source": "<input name="a2">",
    },
  ]
         `);
@@ -43,20 +60,31 @@ generateRuleTests({
     },
     {
       template:
-        '<div><input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey" /></div>',
+        '<div><input id="prefMascot-Zoey"><label for="prefMascot-Zoey" /><input id="prefMascot-tom"><label for="prefMascot-tom" /></div>',
       verifyResults(results) {
         expect(results).toMatchInlineSnapshot(`
  [
    {
      "column": 5,
-     "endColumn": 86,
+     "endColumn": 33,
      "endLine": 1,
      "filePath": "layout.hbs",
      "line": 1,
      "message": "Grouped form controls should have appropriate semantics such as fieldset and legend or WAI-ARIA labels",
      "rule": "require-valid-form-groups",
      "severity": 2,
-     "source": "<input id="radio-001" type="radio" name="prefMascot-Zoey" value="chicago zoey" />",
+     "source": "<input id="prefMascot-Zoey">",
+   },
+   {
+     "column": 64,
+     "endColumn": 91,
+     "endLine": 1,
+     "filePath": "layout.hbs",
+     "line": 1,
+     "message": "Grouped form controls should have appropriate semantics such as fieldset and legend or WAI-ARIA labels",
+     "rule": "require-valid-form-groups",
+     "severity": 2,
+     "source": "<input id="prefMascot-tom">",
    },
  ]
           `);
