b8877de2ca7fb2ab88e4c578d39caa34e8428875
Peter Wagenet <peter@wagenet.us>
2025-08-05 11:31:21 -0700

    Report unusedDisables for globally disabled rules

--- test/fixtures/report-unused-disable-directives/.template-lintrc.cjs
+++ test/fixtures/report-unused-disable-directives/.template-lintrc.cjs
@@ -2,6 +2,16 @@ module.exports = {
   reportUnusedDisableDirectives: true,
   rules: {
     'no-html-comments': 'error',
-    'no-bare-strings': 'error'
-  }
+    'no-bare-strings': 'error',
+    'no-builtin-form-components': 'error',
+  },
+  // Intentionally set up to make sure that we handle overrides correctly
+  overrides: [
+    {
+      files: ['**/*'],
+      rules: {
+        'no-builtin-form-components': 'off',
+      }
+    }
+  ]
 };
